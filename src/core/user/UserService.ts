9923a803c4ce4a6692dd32420b07c24de88c309d
julienlavigne <56667882+julienlavigne@users.noreply.github.com>
2020-04-08 17:16:23 +0100

    ios/android different behaviour (#45)

--- src/core/user/UserService.ts
+++ src/core/user/UserService.ts
@@ -277,7 +277,7 @@ export default class UserService extends ApiClientBase {
     }
 
     public async getAreaStats(patientId: string) {
-        return this.client.get<AreaStatsResponse>(`/area_stats?patient=${patientId}`);
+        return this.client.get<AreaStatsResponse>(`/area_stats/?patient=${patientId}`);
     }
 }
 
