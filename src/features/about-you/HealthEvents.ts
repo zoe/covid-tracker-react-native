b82264d6f2cb0f0d591b4e94b5b0fdad9b59bf18
Somesh Selvachandran <somesh@joinzoe.com>
2020-04-08 09:57:42 +0100

    Remove unused code (#42)

--- src/features/about-you/HealthEvents.ts
+++
@@ -1,5 +0,0 @@
-import {createAction} from 'typesafe-actions';
-import {Health, Symptoms} from "./types";
-
-export const healthFetched = createAction('Health_HealthFetched')<Health>();
-export const symptomsFetched = createAction('Health_SymptomsFetched')<Symptoms>();
