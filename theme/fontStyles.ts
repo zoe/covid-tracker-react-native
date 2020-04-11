274440c3308996d08221a11f67136cb8a61e481c
Somesh Selvachandran <mail@somesh.co.uk>
2020-04-11 11:25:40 +0100

    Remove unused themes

--- theme/fontStyles.ts
+++ theme/fontStyles.ts
@@ -2,81 +2,16 @@ import {StyleSheet} from "react-native";
 import {colors} from "./colors";
 
 export const fontStyles = StyleSheet.create({
-    // headlineLight: {
-    //     fontSize: 64,
-    //     lineHeight: 68,
-    //     color: colors.primary,
-    // },
-    // headlineReg: {
-    //     fontSize: 64,
-    //     lineHeight: 68,
-    //     color: colors.primary,
-    // },
-    // headlineMedium: {
-    //     fontSize: 64,
-    //     lineHeight: 68,
-    //     color: colors.primary,
-    // },
-    // h0Light: {
-    //     fontSize: 40,
-    //     lineHeight: 48,
-    //     color: colors.primary,
-    // },
-    // h0Reg: {
-    //     fontSize: 40,
-    //     lineHeight: 48,
-    //     color: colors.primary,
-    // },
-    // h0Medium: {
-    //     fontSize: 40,
-    //     lineHeight: 48,
-    //     color: colors.primary,
-    // },
-    h1Light: { // HeaderLightText Component - Login & Register Screens
+    h1Light: { // HeaderLightText - Login & Register Screens
         fontSize: 32,
         lineHeight: 48,
         color: colors.primary,
     },
-    // h1Reg: {
-    //     fontSize: 32,
-    //     lineHeight: 48,
-    //     color: colors.primary,
-    // },
-    // h1Medium: {
-    //     fontSize: 32,
-    //     lineHeight: 48,
-    //     color: colors.primary,
-    // },
-    // h2Light: {
-    //     fontSize: 24,
-    //     lineHeight: 32,
-    //     color: colors.primary,
-    // },
     h2Reg: { // HeaderText - DrawerScreen
         fontSize: 24,
         lineHeight: 32,
         color: colors.primary,
     },
-    // h2Medium: {
-    //     fontSize: 24,
-    //     lineHeight: 32,
-    //     color: colors.primary,
-    // },
-    // h3Light: {
-    //     fontSize: 20,
-    //     lineHeight: 32,
-    //     color: colors.primary,
-    // },
-    // h3Reg: {
-    //     fontSize: 20,
-    //     lineHeight: 32,
-    //     color: colors.primary,
-    // },
-    // h3Medium: {
-    //     fontSize: 20,
-    //     lineHeight: 32,
-    //     color: colors.primary,
-    // },
     bodyLight: { // Selector Button
         fontSize: 16,
         lineHeight: 24,
@@ -87,39 +22,15 @@ export const fontStyles = StyleSheet.create({
         lineHeight: 24,
         color: colors.primary,
     },
-    // bodyMedium: {
-    //     fontSize: 16,
-    //     lineHeight: 24,
-    //     color: colors.primary,
-    // },
+    bodyMutedReg: { // Muted Text
+        fontSize: 16,
+        lineHeight: 24,
+        color: colors.tertiary,
+    },
+
     bodySmallLight: { // Caption Text & Version Text
         fontSize: 14,
         lineHeight: 20,
         color: colors.tertiary,
     },
-    // bodySmallReg: {
-    //     fontSize: 14,
-    //     lineHeight: 20,
-    //     color: colors.primary,
-    // },
-    // bodySmallMedium: {
-    //     fontSize: 14,
-    //     lineHeight: 20,
-    //     color: colors.primary,
-    // },
-    // menuLight: {
-    //     fontSize: 12,
-    //     lineHeight: 16,
-    //     color: colors.primary,
-    // },
-    // menuReg: {
-    //     fontSize: 12,
-    //     lineHeight: 16,
-    //     color: colors.primary,
-    // },
-    // menuMedium: {
-    //     fontSize: 12,
-    //     lineHeight: 16,
-    //     color: colors.primary,
-    // }
 });
