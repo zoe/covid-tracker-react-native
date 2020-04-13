d12b1e9556db1950c7f827d955e224fe68b9db7f
Roberta Goodhead <roberta.goodhead@theappbusiness.com>
2020-04-13 15:12:15 +0100

    Removed unused import

--- src/components/__tests__/Button.test.tsx
+++ src/components/__tests__/Button.test.tsx
@@ -1,16 +1,15 @@
 import * as React from "react";
 import renderer from "react-test-renderer";
-import { Text } from "react-native";
 
 import { BigButton } from "../Button";
 
 describe("Button", () => {
   it("should match snapshot", () => {
-    const tree = renderer
+    const component = renderer
       .create(
         <BigButton onPress={() => {}} children={"Button Text"} />
       )
       .toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(component).toMatchSnapshot();
   });
 });
