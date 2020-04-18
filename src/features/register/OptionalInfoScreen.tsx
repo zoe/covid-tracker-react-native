2da146e43812149a9d2e60ccc97b58e39a300ed7
Somesh Selvachandran <mail@somesh.co.uk>
2020-04-18 16:10:05 +0100

    Extract strings for optional info screen

--- src/features/register/OptionalInfoScreen.tsx
+++ src/features/register/OptionalInfoScreen.tsx
@@ -14,11 +14,11 @@ import {PiiRequest} from "../../core/user/dto/UserAPIContracts";
 import {PushNotificationService} from "../../core/PushNotificationService";
 import {AsyncStorageService} from "../../core/AsyncStorageService";
 import Constants from 'expo-constants';
+import i18n from "../../locale/i18n";
 
 type PropsType = {
     navigation: StackNavigationProp<ScreenParamList, 'OptionalInfo'>
     route: RouteProp<ScreenParamList, 'OptionalInfo'>;
-
 }
 
 type State = {
@@ -76,7 +76,7 @@ export class OptionalInfoScreen extends Component<PropsType, State> {
                     await userService.savePushToken(pushToken);
                     AsyncStorageService.setPushToken(pushToken);
                 } catch (error) {
-                    this.setState({errorMessage: 'Something went wrong... try again later'});
+                    this.setState({errorMessage: i18n.t("something-went-wrong")});
                 }
             }
         }
@@ -101,11 +101,10 @@ export class OptionalInfoScreen extends Component<PropsType, State> {
                 .then(() => {
                     goToNextScreen()
                 }).catch(err => {
-                this.setState({errorMessage: 'Something went wrong... try again later'});
+                this.setState({errorMessage: i18n.t("something-went-wrong")});
                 console.log(err.response.data);
             })
         }
-
     }
 
     // see http://regexlib.com/(X(1)A(TahywDmnNCw0iyuu7jNEB2AWTPaTyZQd-r8XZECVzmio5oP08fV7JoAWrNnIoyH3vysaiCJYtQO_FfuRAXJRSwB8zqAr_L9ddGD5V0eCJcVBJ65SiOnOt1tLVw4pd_Q3Q0FoUOPG5fXsbR6DHK6jqtBaIxnP0NL5oevVH6y0uSXhYgbCrrRx1DeE-59F0s5i0))/UserPatterns.aspx?authorid=d95177b0-6014-4e73-a959-73f1663ae814&AspxAutoDetectCookieSupport=1
@@ -129,16 +128,16 @@ export class OptionalInfoScreen extends Component<PropsType, State> {
                                 <View>
 
                                     <View>
-                                        <HeaderText style={{marginBottom: 24}}>Optional contact information</HeaderText>
+                                        <HeaderText style={{marginBottom: 24}}>{i18n.t("optional-info-title")}</HeaderText>
 
                                         <RegularText>
-                                            Please share your name and phone number. This is optional. If you want to share your information with another study this will help us identify you.
+                                            {i18n.t("optional-info-description")}
                                         </RegularText>
 
                                         <Form>
 
                                             <ValidatedTextInput
-                                                placeholder="Your name"
+                                                placeholder={i18n.t("optional-info-name-placeholder")}
                                                 value={props.values.name}
                                                 onChangeText={props.handleChange("name")}
                                                 onBlur={props.handleBlur("name")}
@@ -151,7 +150,7 @@ export class OptionalInfoScreen extends Component<PropsType, State> {
 
                                             <ValidatedTextInput
                                                 ref={(input) => this.phoneComponent = input}
-                                                placeholder="Phone number"
+                                                placeholder={i18n.t("optional-info-phone-placeholder")}
                                                 value={props.values.phone}
                                                 onChangeText={props.handleChange("phone")}
                                                 onBlur={props.handleBlur("phone")}
@@ -167,7 +166,7 @@ export class OptionalInfoScreen extends Component<PropsType, State> {
                                     </View>
 
                                     <View>
-                                        <BrandedButton onPress={props.handleSubmit}>Continue</BrandedButton>
+                                        <BrandedButton onPress={props.handleSubmit}>{i18n.t("optional-info-button")}</BrandedButton>
                                     </View>
                                 </View>
                             );
