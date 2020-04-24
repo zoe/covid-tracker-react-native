90650d3f706f59c6ff3d1fb685e0515790c7d62d
James Brooks <james@brookode.uk>
2020-04-24 20:53:45 +0100

    Add BrandedButton tests

---
+++ src/components/BrandedButton/index.tsx
@@ -0,0 +1,60 @@
+import { Button } from "native-base";
+import React from "react";
+import { ActivityIndicator, StyleSheet, Text } from "react-native";
+
+import { colors, fontStyles } from "../../../theme";
+import { ClickableProps } from "../Text";
+
+interface BrandedButtonProps {
+  buttonProps?: any;
+  textProps?: any;
+}
+
+export const BrandedButton = ({
+  style,
+  children,
+  onPress,
+  enable,
+  buttonProps,
+  textProps,
+  hideLoading,
+}: ClickableProps & BrandedButtonProps) => {
+  const btnStyle = [styles.button, style];
+  const isDisabled = enable === false;
+  if (isDisabled) {
+    btnStyle.push(styles.buttonDisabled);
+  }
+
+  return (
+    <Button
+      block
+      style={btnStyle}
+      onPress={() => {
+        !isDisabled && onPress();
+      }}
+      {...buttonProps}
+    >
+      <Text style={[fontStyles.bodyLight, styles.buttonText]} {...textProps}>
+        {children}
+      </Text>
+      {isDisabled && !hideLoading && <ActivityIndicator size="small" />}
+    </Button>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 100,
+    height: 56,
+    backgroundColor: colors.brand,
+    elevation: 0,
+  },
+
+  buttonDisabled: {
+    backgroundColor: colors.backgroundBrandDisabled,
+  },
+
+  buttonText: {
+    color: colors.white,
+  },
+});
