75c38f927b35da5f5b4d2e0212cc19ab4c97a75c
James Brooks <james@brookode.uk>
2020-04-24 23:05:50 +0100

    Fix US translation

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -1,10 +1,13 @@
-import i18n from "i18n-js";
-
+import i18n from 'i18n-js';
+import { mergeDeep } from 'immutable';
 i18n.fallbacks = true;
+
+const en = require('../../assets/lang/en.json');
+
 i18n.translations = {
-    "en": require('../../assets/lang/en.json'),
-    "en-US": require('../../assets/lang/en-US.json'),
-    "sv-SE": require('../../assets/lang/sv-SE.json')
+  en,
+  'en-US': mergeDeep(en, require('../../assets/lang/en-US.json')),
+  'sv-SE': require('../../assets/lang/sv-SE.json'),
 };
 
 export default i18n;
