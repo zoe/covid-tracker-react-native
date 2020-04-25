22883a9d7000278ce058024e8ffa8dcd06aa75e8
Josh Elgar <joshelgar@gmail.com>
2020-04-25 16:03:27 +0100

    SWEDEN consent flow 4 (#167)
    * Merge branch 'sv-support' into swedish_consent_flow
    
    # Conflicts:
    #	assets/index.ts
    #	assets/lang/en-US.json
    #	assets/lang/sv-SE.json
    #	assets/svMap.png
    #	package-lock.json
    #	src/CovidApp.tsx
    #	src/core/user/UserService.ts
    #	src/features/CountrySelectScreen.tsx
    #	src/features/ScreenParamList.tsx
    #	src/features/register/ConsentScreen.tsx
    #	src/features/register/CountryIpModal.tsx
    #	src/features/register/Welcome1Screen.tsx
    #	src/features/register/Welcome2Screen.tsx
    #	src/features/register/WelcomeRepeatScreen.tsx
    
    * Fix typo
    
    * Consent screen + legal components
    
    * Swedish consent button
    
    * Add checkbox consent check.
    
    * Change document from SV to SE
    
    * Constants naming.
    
    * Update src/features/register/ConsentScreen.tsx
    
    Insert Ethics Approval Number instead of placeholder.
    
    * Refactor checkboxes.
    
    * legal i18n strings.
    
    * Refactor consent screen some more.
    
    * Remove unnecessary remnant country check
    
    Co-authored-by: Josh Elgar <josh@joinzoe.com>
    Co-authored-by: Abu Buwe <52913482+abubuwe@users.noreply.github.com>

---
+++ src/features/register/LegalComponents.tsx
@@ -0,0 +1,14 @@
+import { RegularBoldText, RegularText } from "../../components/Text";
+import React from "react";
+
+export const HeaderText = (props: { text: string }) => {
+    return (
+        <RegularBoldText>
+            {props.text}{"\n"}
+        </RegularBoldText>
+    )
+};
+
+export const SimpleTextBlock = (props: {text: string[], separator?: string}) => <RegularText>{props.text.map(t => `${t}${props.separator || `\n\n`}`)}</RegularText>;
+
+export const BulletedTextBlock = (props: {text: string[]}) =>  <RegularText>{props.text.map(t => `- ${t}\n`)}</RegularText>;
