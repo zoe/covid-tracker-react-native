b15b391d1dc39a63875c3f3ce164a62759870bdd
James Brooks <jamesbrooks94@users.noreply.github.com>
2020-05-02 18:49:05 +0100

    test(BrandedButton): Add some unit tests (#169)
    * Add enzyme and update button test
    
    * Add BrandedButton tests
    
    * Update BrandedButton.test.native.tsx
    
    * Update index.tsx
    
    Co-authored-by: Somesh Selvachandran <somesh@joinzoe.com>

---
+++ src/components/BrandedButton/__tests__/BrandedButton.test.native.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { BrandedButton } from '..';
+
+describe('BrandedButton tests', () => {
+  it('Handles press events', () => {
+    const onPress = jest.fn();
+    const component = shallow(<BrandedButton onPress={onPress}>This is a branded button</BrandedButton>);
+    expect(onPress).toHaveBeenCalledTimes(0);
+    component.simulate('press');
+    expect(onPress).toHaveBeenCalledTimes(1);
+  });
+  it('Does not fire press events when disabled', () => {
+    const onPress = jest.fn();
+    const component = shallow(
+      <BrandedButton onPress={onPress} enable={false}>
+        This is a branded button
+      </BrandedButton>
+    );
+    expect(onPress).toHaveBeenCalledTimes(0);
+    component.simulate('press');
+    expect(onPress).toHaveBeenCalledTimes(0);
+  });
+  it('Handles style props being passed', () => {
+    const onPress = jest.fn();
+    const style = { background: 'red' };
+    const component = shallow(
+      <BrandedButton style={style} onPress={onPress} enable={false}>
+        This is a branded button
+      </BrandedButton>
+    );
+    expect(component.props().style[1]).toBe(style);
+  });
+  it('Shows loading if disabled', () => {
+    const onPress = jest.fn();
+    const component = shallow(
+      <BrandedButton onPress={onPress} enable={false}>
+        This is a branded button
+      </BrandedButton>
+    );
+    expect(component.find('ActivityIndicator')).toHaveLength(1);
+  });
+  it('Can hide loading when disabled', () => {
+    const onPress = jest.fn();
+    const component = shallow(
+      <BrandedButton onPress={onPress} enable={false} hideLoading>
+        This is a branded button
+      </BrandedButton>
+    );
+    expect(component.find('ActivityIndicator')).toHaveLength(0);
+  });
+});
