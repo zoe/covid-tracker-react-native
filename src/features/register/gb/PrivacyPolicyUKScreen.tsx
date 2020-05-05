2824c4a02e04df2b9390eda200372e7b0d5d2484
Mike Davies <miked@joinzoe.com>
2020-05-05 16:51:09 +0100

    Prettier 2.

--- src/features/register/gb/PrivacyPolicyUKScreen.tsx
+++ src/features/register/gb/PrivacyPolicyUKScreen.tsx
@@ -122,7 +122,7 @@ export class PrivacyPolicyUKScreen extends Component<PropsType, object> {
               'Intercom',
               'Sentry',
               'Cloudflare',
-              'Sqreen'
+              'Sqreen',
             ]}
           />
 
