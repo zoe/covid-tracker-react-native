9ad5524666cd16abd59ee5d9fd4d82670021f4e0
Kenneth Read <kenneth@joinzoe.com>
2020-05-14 18:05:17 +0100

    Add dual language set from locale

--- src/locale/i18n.ts
+++ src/locale/i18n.ts
@@ -4,6 +4,7 @@ i18n.fallbacks = true;
 i18n.translations = {
   en: require('../../assets/lang/en.json'),
   'en-GB': require('../../assets/lang/en-GB.json'),
+  'es': require('../../assets/lang/es.json'),
   'en-US': require('../../assets/lang/en-US.json'),
   'sv-SE': require('../../assets/lang/sv-SE.json'),
 };
