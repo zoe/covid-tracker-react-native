86d04b8bdd0e35cda0119b5a898be0bc36cf45ac
Benjamin Piggin <56440821+bpiggin@users.noreply.github.com>
2020-05-17 21:19:51 +0100

    add tick (#284)
    Co-authored-by: Somesh Selvachandran <somesh@joinzoe.com>

--- src/components/DaysAgo.tsx
+++ src/components/DaysAgo.tsx
@@ -2,22 +2,25 @@ import moment from 'moment';
 import React, { Component } from 'react';
 
 import i18n from '../locale/i18n';
-import { RegularText, SecondaryText } from './Text';
+import { SecondaryText } from './Text';
 
 type ProgressProps = {
   timeAgo: Date | undefined;
 };
 
+export const getDaysAgo = (date: Date) => {
+  const today = moment().utc().startOf('day');
+  return today.diff(moment(date).startOf('day'), 'days');
+};
+
 export default class DaysAgo extends Component<ProgressProps> {
   render() {
     let text = i18n.t('never-reported');
-
     if (this.props.timeAgo) {
-      const today = moment().utc().startOf('day');
-      const diffDays = today.diff(moment(this.props.timeAgo).startOf('day'), 'days');
-      if (diffDays == 0) {
+      const diffDays = getDaysAgo(this.props.timeAgo);
+      if (diffDays === 0) {
         text = i18n.t('today');
-      } else if (diffDays == 1) {
+      } else if (diffDays === 1) {
         text = i18n.t('yesterday');
       } else {
         text = i18n.t('days-ago', { diffDays });
