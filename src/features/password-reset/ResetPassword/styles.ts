4839b09808a5675a0bac0147066bf4d2faced4f3
Massimo Necchi <massimo.necchi@gmail.com>
2020-05-19 17:31:01 +0100

    refactor of ResetPassword to make it more easily testable

---
+++ src/features/password-reset/ResetPassword/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+import { colors } from '../../../../theme';
+
+export default StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: colors.backgroundPrimary,
+    paddingHorizontal: 24,
+    paddingTop: 56,
+  },
+  formItem: {
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+  },
+});
