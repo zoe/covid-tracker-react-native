182d1ce0bfc9f1820da6667edaced73ca5b88686
Abu Buwe <abu@joinzoe.com>
2020-05-23 00:22:14 +0100

    add error svg

---
+++ assets/icons/Error.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Svg, { Circle, Path } from 'react-native-svg';
+
+type CheckProps = {
+  width?: number | string;
+  height?: number | string;
+};
+
+const Error: React.FC<CheckProps> = ({ width = 24, height = 24 }) => {
+  return (
+    <Svg width={width} height={height} fill="none" viewBox={`0 0 24 24`}>
+      <Circle cx="12" cy="12" r="12" fill="#FA7268" />
+      <Path
+        stroke="white"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M15.1819 15.1819L8.81803 8.81802"
+      />
+      <Path
+        stroke="white"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="1.5"
+        d="M8.81803 15.1819L15.1819 8.81802"
+      />
+    </Svg>
+  );
+};
+
+export default Error;
