75a364b99c58be00b852b4a532f3511151414897
Somesh Selvachandran <somesh@joinzoe.com>
2020-05-29 20:14:58 +0100

    Pass down consent_validation so we can block folks who haven't updated to latest in app consent (#357)

--- src/features/register/constants.ts
+++ src/features/register/constants.ts
@@ -5,3 +5,4 @@ export const consentVersionSE = 'v1';
 export const privacyPolicyVersionUS = 'v1.3';
 export const privacyPolicyVersionUK = 'v2.3';
 export const privacyPolicyVersionSE = 'v2.1';
+export const ukValidationStudyConsentVersion = 'v3';
