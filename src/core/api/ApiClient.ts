028f7e280cc9867cf39ab03ee3a0665d8aa46776
Mike Davies <miked@joinzoe.com>
2020-06-02 15:11:48 +0100

    Add PATCH to API Client

--- src/core/api/ApiClient.ts
+++ src/core/api/ApiClient.ts
@@ -3,6 +3,7 @@ import { handleServiceError } from './ApiServiceErrors';
 
 export interface IApiClient {
   post<TRequest, TResponse>(path: string, object: TRequest): Promise<TResponse>;
+  patch<TRequest, TResponse>(path: string, object: TRequest): Promise<TResponse>;
 }
 
 export default class ApiClient extends ApiClientBase implements IApiClient {
@@ -11,6 +12,16 @@ export default class ApiClient extends ApiClientBase implements IApiClient {
   async post<TRequest, TResponse>(path: string, payload: TRequest): Promise<TResponse> {
     try {
       const response = await this.client.post<TResponse>(path, payload);
+      return response.data;
+    } catch (error) {
+      handleServiceError(error);
+    }
+    return {} as TResponse;
+  }
+
+  async patch<TRequest, TResponse>(path: string, payload: TRequest): Promise<TResponse> {
+    try {
+      const response = await this.client.patch<TResponse>(path, payload);
       return response.data;
     } catch (error) {
       handleServiceError(error);
