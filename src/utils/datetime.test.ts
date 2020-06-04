f3352033971ffc156811395f268756c52e91d616
Mike Davies <miked@joinzoe.com>
2020-06-04 14:29:29 +0100

    Fix date calculation to get today and yesterday correct.

--- src/utils/datetime.test.ts
+++ src/utils/datetime.test.ts
@@ -10,7 +10,14 @@ const utcComparisons = [
 
 const pacificComparisons = [
   { startDate: '2020-05-05T11:13:29-07:00', endDate: '2020-05-05T11:13:29Z', expected: 0 },
-  { startDate: '2020-05-05T18:13:29-07:00', endDate: '2020-05-05T18:13:29Z', expected: 0 },
+  { startDate: '2020-05-05T18:13:29-07:00', endDate: '2020-05-05T18:13:29Z', expected: 1 },
+];
+
+const daylightSavingsTimes = [
+  { startDate: '2020-06-04T08:00:00+01:00', endDate: '2020-06-03T23:59:59+01:00', expected: 1 },
+  { startDate: '2020-06-04T08:00:00+01:00', endDate: '2020-06-03T23:59:59Z', expected: 0 },
+  { startDate: '2020-06-04T08:00:00+01:00', endDate: '2020-06-03T22:59:59Z', expected: 1 },
+  { startDate: '2020-06-04T08:00:00+01:00', endDate: '2020-06-03T21:59:59Z', expected: 1 },
 ];
 
 describe('getDaysAgo', () => {
@@ -22,6 +29,12 @@ describe('getDaysAgo', () => {
   });
   it('works in pacific timezones', () => {
     pacificComparisons.forEach((test) => {
+      const { startDate, endDate, expected } = test;
+      expect(calcDaysDiff(new Date(startDate), new Date(endDate))).toBe(expected);
+    });
+  });
+  it('calculates yesterday correctly', () => {
+    daylightSavingsTimes.forEach((test) => {
       const { startDate, endDate, expected } = test;
       expect(calcDaysDiff(new Date(startDate), new Date(endDate))).toBe(expected);
     });
