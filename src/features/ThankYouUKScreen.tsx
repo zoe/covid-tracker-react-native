3d02f650e0d6236e9b55fa047884327c258db9f3
Somesh Selvachandran <somesh@joinzoe.com>
2020-06-11 16:20:00 +0100

    Remove experiments and survey from thank you page (#407)
    * Remove experiments and survey from thank you page
    
    * Update Blog and Incidence modules

--- src/features/ThankYouUKScreen.tsx
+++ src/features/ThankYouUKScreen.tsx
@@ -4,7 +4,7 @@ import { Text } from 'native-base';
 import React, { Component } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
-import { blog001, incidence001, surveyInvite, webinar001 } from '@assets';
+import { blog002, incidence002, webinar001 } from '@assets';
 import { colors } from '@theme';
 import { CovidRating, shouldAskForRating } from '@covid/components/CovidRating';
 import { ExternalCallout } from '@covid/components/ExternalCallout';
@@ -12,7 +12,6 @@ import InviteToStudy from '@covid/components/InviteToStudy';
 import { Header } from '@covid/components/Screen';
 import ShareThisApp from '@covid/components/ShareThisApp';
 import { BrandedButton, ClickableText, HeaderText, RegularText } from '@covid/components/Text';
-import { experiments, startExperiment } from '@covid/core/Experiments';
 import i18n from '@covid/locale/i18n';
 import { userService } from '@covid/Services';
 
@@ -26,13 +25,11 @@ type RenderProps = {
 type State = {
   askForRating: boolean;
   inviteToStudy: boolean;
-  variant: string | null;
 };
 
 const initialState = {
   askForRating: false,
   inviteToStudy: false,
-  variant: null,
 };
 
 export default class ThankYouUKScreen extends Component<RenderProps, State> {
@@ -41,15 +38,11 @@ export default class ThankYouUKScreen extends Component<RenderProps, State> {
   async componentDidMount() {
     this.setState({
       askForRating: await shouldAskForRating(),
-      variant: await startExperiment(experiments.Experiment_001, 4),
       inviteToStudy: await userService.shouldAskForValidationStudy(true),
     });
   }
 
   render() {
-    const showIncidenceCallout = this.state.variant === 'variant_1' || this.state.variant === 'variant_4';
-    const showWebinarCallout = this.state.variant === 'variant_2' || this.state.variant === 'variant_4';
-    const showBlogCallout = this.state.variant === 'variant_3' || this.state.variant === 'variant_4';
     return (
       <>
         {this.state.askForRating && <CovidRating />}
@@ -64,38 +57,25 @@ export default class ThankYouUKScreen extends Component<RenderProps, State> {
                 <RegularText style={styles.subTitle}>{i18n.t('thank-you-uk.subtitle')}</RegularText>
               </View>
 
-              {showIncidenceCallout && (
-                <ExternalCallout
-                  link="https://covid.joinzoe.com/data#daily-new-cases?utm_source=App"
-                  calloutID="incidence_001"
-                  imageSource={incidence001}
-                  aspectRatio={1.5}
-                />
-              )}
-
-              {showWebinarCallout && (
-                <ExternalCallout
-                  link="https://youtu.be/oAmVPaxMQ1c"
-                  calloutID="webinar_001"
-                  imageSource={webinar001}
-                  aspectRatio={1.178}
-                />
-              )}
-
-              {showBlogCallout && (
-                <ExternalCallout
-                  link="https://covid.joinzoe.com/post/science-covid-diagnosis?utm_source=App"
-                  calloutID="blog_001"
-                  imageSource={blog001}
-                  aspectRatio={1.551}
-                />
-              )}
+              <ExternalCallout
+                link="https://covid.joinzoe.com/data#daily-new-cases?utm_source=App"
+                calloutID="incidence_002"
+                imageSource={incidence002}
+                aspectRatio={1.5}
+              />
 
               <ExternalCallout
-                link="https://www.surveymonkey.co.uk/r/LC26RN9"
-                calloutID="surveyInvite"
-                imageSource={surveyInvite}
-                aspectRatio={3.38}
+                link="https://youtu.be/oAmVPaxMQ1c"
+                calloutID="webinar_001"
+                imageSource={webinar001}
+                aspectRatio={1.178}
+              />
+
+              <ExternalCallout
+                link="https://covid.joinzoe.com/post/covid-long-term?utm_source=App"
+                calloutID="blog_002"
+                imageSource={blog002}
+                aspectRatio={1.551}
               />
 
               <View style={{ margin: 10 }} />
