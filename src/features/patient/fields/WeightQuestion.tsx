d5d8948c6904820caf190188311e94e2e64132b1
Abu Buwe <52913482+abubuwe@users.noreply.github.com>
2020-06-16 10:21:44 +0100

    Update textfield (#423)
    * fit component with correct margins to prevent spill
    
    * adding background/color styling
    
    * fix alignment of questions with unit dropdowns
    
    * switch to new Error icon
    
    * remove unused imports

--- src/features/patient/fields/WeightQuestion.tsx
+++ src/features/patient/fields/WeightQuestion.tsx
@@ -1,5 +1,4 @@
 import { FormikProps } from 'formik';
-import { Item, Label } from 'native-base';
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 
@@ -10,6 +9,7 @@ import { ValidationError } from '@covid/components/ValidationError';
 import { isUSCountry } from '@covid/core/user/UserService';
 import i18n from '@covid/locale/i18n';
 import { userService } from '@covid/Services';
+import { RegularText } from '@covid/components/Text';
 
 export interface WeightData {
   weight: string;
@@ -36,77 +36,75 @@ export class WeightQuestion extends Component<Props, object> {
   render() {
     const props = this.props.formikProps;
     return (
-      <FieldWrapper>
-        <Item stackedLabel style={styles.textItemStyle}>
-          <Label>{i18n.t('your-weight')}</Label>
-          {isUSCountry() ? (
-            <ValidatedTextInput
-              placeholder={i18n.t('placeholder-pounds')}
-              value={props.values.pounds}
-              onChangeText={props.handleChange('pounds')}
-              onBlur={props.handleBlur('pounds')}
-              error={props.touched.pounds && props.errors.pounds}
-              returnKeyType="next"
-              onSubmitEditing={() => {}}
-              keyboardType="numeric"
-            />
-          ) : (
-            <View style={styles.fieldRow}>
-              {props.values.weightUnit === 'kg' ? (
-                <View style={styles.primaryField}>
+      <FieldWrapper style={styles.fieldWrapper}>
+        <RegularText>{i18n.t('your-weight')}</RegularText>
+        {isUSCountry() ? (
+          <ValidatedTextInput
+            placeholder={i18n.t('placeholder-pounds')}
+            value={props.values.pounds}
+            onChangeText={props.handleChange('pounds')}
+            onBlur={props.handleBlur('pounds')}
+            error={props.touched.pounds && props.errors.pounds}
+            returnKeyType="next"
+            onSubmitEditing={() => {}}
+            keyboardType="numeric"
+          />
+        ) : (
+          <View style={styles.fieldRow}>
+            {props.values.weightUnit === 'kg' ? (
+              <View style={styles.primaryField}>
+                <ValidatedTextInput
+                  placeholder={i18n.t('placeholder-weight')}
+                  value={props.values.weight}
+                  onChangeText={props.handleChange('weight')}
+                  onBlur={props.handleBlur('weight')}
+                  error={props.touched.weight && props.errors.weight}
+                  returnKeyType="next"
+                  onSubmitEditing={() => {}}
+                  keyboardType="numeric"
+                />
+              </View>
+            ) : (
+              <View style={styles.primaryFieldRow}>
+                <View style={styles.tertiaryField}>
                   <ValidatedTextInput
-                    placeholder={i18n.t('placeholder-weight')}
-                    value={props.values.weight}
-                    onChangeText={props.handleChange('weight')}
-                    onBlur={props.handleBlur('weight')}
-                    error={props.touched.weight && props.errors.weight}
+                    placeholder={i18n.t('placeholder-stones')}
+                    value={props.values.stones}
+                    onChangeText={props.handleChange('stones')}
+                    onBlur={props.handleBlur('stones')}
+                    error={props.touched.stones && props.errors.stones}
                     returnKeyType="next"
                     onSubmitEditing={() => {}}
                     keyboardType="numeric"
                   />
                 </View>
-              ) : (
-                <View style={styles.primaryFieldRow}>
-                  <View style={styles.tertiaryField}>
-                    <ValidatedTextInput
-                      placeholder={i18n.t('placeholder-stones')}
-                      value={props.values.stones}
-                      onChangeText={props.handleChange('stones')}
-                      onBlur={props.handleBlur('stones')}
-                      error={props.touched.stones && props.errors.stones}
-                      returnKeyType="next"
-                      onSubmitEditing={() => {}}
-                      keyboardType="numeric"
-                    />
-                  </View>
-                  <View style={styles.tertiaryField}>
-                    <ValidatedTextInput
-                      placeholder={i18n.t('placeholder-pounds')}
-                      value={props.values.pounds}
-                      onChangeText={props.handleChange('pounds')}
-                      onBlur={props.handleBlur('pounds')}
-                      error={props.touched.pounds && props.errors.pounds}
-                      returnKeyType="next"
-                      onSubmitEditing={() => {}}
-                      keyboardType="numeric"
-                    />
-                  </View>
+                <View style={styles.tertiaryField}>
+                  <ValidatedTextInput
+                    placeholder={i18n.t('placeholder-pounds')}
+                    value={props.values.pounds}
+                    onChangeText={props.handleChange('pounds')}
+                    onBlur={props.handleBlur('pounds')}
+                    error={props.touched.pounds && props.errors.pounds}
+                    returnKeyType="next"
+                    onSubmitEditing={() => {}}
+                    keyboardType="numeric"
+                  />
                 </View>
-              )}
-              <View style={styles.secondaryField}>
-                <DropdownField
-                  onlyPicker
-                  selectedValue={props.values.weightUnit}
-                  onValueChange={props.handleChange('weightUnit')}
-                  items={[
-                    { label: 'lbs', value: 'lbs' },
-                    { label: 'kg', value: 'kg' },
-                  ]}
-                />
               </View>
+            )}
+            <View style={styles.secondaryField}>
+              <DropdownField
+                onlyPicker
+                selectedValue={props.values.weightUnit}
+                onValueChange={props.handleChange('weightUnit')}
+                items={[
+                  { label: 'lbs', value: 'lbs' },
+                  { label: 'kg', value: 'kg' },
+                ]}
+              />
             </View>
-          )}
-        </Item>
+          </View>
+        )}
         {props.touched.weight && props.errors.weight && <ValidationError error={props.errors.weight} />}
         {props.touched.pounds && props.errors.pounds && <ValidationError error={props.errors.pounds} />}
         {props.touched.stones && props.errors.stones && <ValidationError error={props.errors.stones} />}
@@ -117,6 +115,11 @@ export class WeightQuestion extends Component<Props, object> {
 }
 
 const styles = StyleSheet.create({
+  fieldWrapper: {
+    flex: 1,
+    marginHorizontal: 16,
+  },
+
   fieldRow: {
     flexDirection: 'row',
   },
@@ -141,5 +144,6 @@ const styles = StyleSheet.create({
 
   secondaryField: {
     flex: 2,
+    margin: -8,
   },
 });
