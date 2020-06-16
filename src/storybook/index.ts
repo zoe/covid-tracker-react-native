296e232b7e3abc93daa012897b0baa71981ed11f
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-06-16 14:43:42 +0100

    Feature/updated dropdown field (#422)
    * Convert DropdownField from class to FC
    
    * Added Stories for DropdownField
    
    * Use different key for dropdown placeholder
    
    * Clean up
    
    * Updated lib for rn modal dropdown
    
    * Added stroies
    
    * rename method
    
    * Changed from feedback
    
    * Minor icon changes
    
    * Cleaner code
    
    * Updated icon color
    
    Co-authored-by: Chuck Ng <chuckng@chucks-mbp-zoe.mynet>
    Co-authored-by: Somesh Selvachandran <somesh@joinzoe.com>

--- src/storybook/index.ts
+++ src/storybook/index.ts
@@ -10,6 +10,7 @@ configure(() => {
   require('./stories/PoweredByZoe.stories');
   require('./stories/Spinner.stories');
   require('./stories/VisitWebsite.stories');
+  require('./stories/DropdownField.stories');
 }, module);
 
 // Global Decorator
@@ -17,6 +18,8 @@ addDecorator(CenterView);
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({
+  asyncStorage: null,
+});
 
 export default StorybookUIRoot;
