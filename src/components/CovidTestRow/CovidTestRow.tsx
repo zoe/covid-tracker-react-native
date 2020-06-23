13ee3438be2eb083bb128def24c6008205181f9e
Somesh Selvachandran <mail@somesh.co.uk>
2020-06-23 14:09:32 +0100

    Refactor date rendering to use moment.

--- src/components/CovidTestRow/CovidTestRow.tsx
+++ src/components/CovidTestRow/CovidTestRow.tsx
@@ -7,14 +7,14 @@ import { RegularText } from '@covid/components/Text';
 import { chevronRight, pending, tick } from '@assets';
 import { CovidTest } from '@covid/core/user/dto/CovidTestContracts';
 import { colors } from '@theme';
-import i18n, { getDayName, getMonthName } from '@covid/locale/i18n';
+import i18n from '@covid/locale/i18n';
 
 type Props = {
   item: CovidTest;
 };
 
 export const CovidTestRow: React.FC<Props> = ({ item }) => {
-  const resultString = (result: string) => {
+  const formatTestResult = (result: string) => {
     switch (result) {
       case 'positive':
         return i18n.t('covid-test-list.positive');
@@ -27,31 +27,30 @@ export const CovidTestRow: React.FC<Props> = ({ item }) => {
     }
   };
 
-  const icon = (result: string) => {
-    if (result === 'waiting') {
-      return pending;
-    } else {
-      return tick;
-    }
+  const getRowIcon = (result: string) => {
+    return result === 'waiting' ? pending : tick;
   };
 
-  const dateString = (test: CovidTest) => {
+  const formatDateString = (dateString: string): string => {
+    return moment(dateString).format('MMM d');
+  };
+
+  const formatTestDate = (test: CovidTest) => {
     if (test.date_taken_specific) {
-      const date = moment(test.date_taken_specific).toDate();
-      return `${getMonthName(date)} ${date.getDate()} (${getDayName(date)})`;
+      return formatDateString(test.date_taken_specific);
     } else {
-      const stateDate = moment(test.date_taken_between_start).toDate();
-      const endDate = moment(test.date_taken_between_end).toDate();
-      return `${getMonthName(stateDate)} ${stateDate.getDate()} - ${getMonthName(endDate)} ${endDate.getDate()}`;
+      return `${formatDateString(test.date_taken_between_start)} - ${formatDateString(test.date_taken_between_end)}`;
     }
   };
 
   return (
     <TouchableOpacity style={styles.itemTouchable} onPress={() => AssessmentCoordinator.goToAddEditTest(item)}>
-      <Image source={icon(item.result)} style={styles.tick} />
-      <RegularText style={item.result === 'waiting' ? styles.pendingText : []}>{dateString(item)}</RegularText>
+      <Image source={getRowIcon(item.result)} style={styles.tick} />
+      <RegularText style={[item.result === 'waiting' && styles.pendingText]}>{formatTestDate(item)}</RegularText>
       <View style={{ flex: 1 }} />
-      <RegularText style={item.result === 'waiting' ? styles.pendingText : []}>{resultString(item.result)}</RegularText>
+      <RegularText style={[item.result === 'waiting' && styles.pendingText]}>
+        {formatTestResult(item.result)}
+      </RegularText>
       <Image source={chevronRight} style={styles.chevron} />
     </TouchableOpacity>
   );
