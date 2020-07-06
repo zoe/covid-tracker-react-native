b04348e730589ac12b0a4b2bfd8522ef6010cbc5
Somesh Selvachandran <mail@somesh.co.uk>
2020-07-06 21:13:30 +0100

    Add shell screens

---
+++ src/features/diet-study/DietStudyAboutYouScreen.tsx
@@ -0,0 +1,54 @@
+import { RouteProp } from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+
+import ProgressStatus from '@covid/components/ProgressStatus';
+import Screen, { Header, ProgressBlock } from '@covid/components/Screen';
+import { HeaderText } from '@covid/components/Text';
+import AssessmentCoordinator from '@covid/core/assessment/AssessmentCoordinator';
+import { assessmentService } from '@covid/Services';
+import { ScreenParamList } from '@covid/features/ScreenParamList';
+
+type Props = {
+  navigation: StackNavigationProp<ScreenParamList, 'DietStudyAboutYou'>;
+  route: RouteProp<ScreenParamList, 'DietStudyAboutYou'>;
+};
+
+export default class DietStudyAboutYouScreen extends Component<Props> {
+  constructor(props: Props) {
+    super(props);
+    AssessmentCoordinator.resetNavigation(props.navigation);
+  }
+
+  private async updateAssessment(status: string, isComplete = false) {
+    const { assessmentId } = AssessmentCoordinator.assessmentData;
+    const assessment = {
+      location: status,
+    };
+
+    if (isComplete) {
+      await assessmentService.completeAssessment(assessmentId!, assessment);
+    } else {
+      await assessmentService.saveAssessment(assessmentId!, assessment);
+    }
+  }
+
+  render() {
+    const currentPatient = AssessmentCoordinator.assessmentData.currentPatient;
+
+    return (
+      <Screen profile={currentPatient.profile} navigation={this.props.navigation}>
+        <Header>
+          <HeaderText>{}</HeaderText>
+        </Header>
+
+        <ProgressBlock>
+          <ProgressStatus step={1} maxSteps={3} />
+        </ProgressBlock>
+      </Screen>
+    );
+  }
+}
+
+const styles = StyleSheet.create({});
