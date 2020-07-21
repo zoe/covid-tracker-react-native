90a5683aee1751224a391fa80b041f3ce63916b0
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-07-21 10:38:05 +0100

    chore: remove comments

--- src/features/DrawerMenu.tsx
+++ src/features/DrawerMenu.tsx
@@ -183,8 +183,6 @@ export function DrawerMenu(props: DrawerContentComponentProps) {
           <HeaderText>Open all FFQ</HeaderText>
           <Switch
             trackColor={{ false: '#767577', true: '#81b0ff' }}
-            // thumbColor={userService.openAllFFQ ? "#f5dd4b" : "#f4f3f4"}
-            // ios_backgroundColor="#3e3e3e"
             onValueChange={(value) => {
               userService.toggleOpenAllFFQ();
               setOpenAllFFQ(userService.openAllFFQ);
