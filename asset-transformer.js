aefea3c97eb2629f44ef4264edb2872c7d1855f4
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-08-03 14:04:42 +0100

    style: clean up

--- asset-transformer.js
+++ asset-transformer.js
@@ -1,8 +1,4 @@
-// For React Native version 0.59 or later
-// var upstreamTransformer = require("metro-react-native-babel-transformer");
-// var sassTransformer = require("react-native-sass-transformer");
 var svgTransformer = require('react-native-svg-transformer');
-// require.resolve('react-native-typescript-transformer')
 var rnTSTransformer = require('react-native-typescript-transformer');
 
 module.exports.transform = function ({ src, filename, options }) {
