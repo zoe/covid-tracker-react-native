1a2481d346d0c6e54a1a73fb2cacc28e5f03c75a
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-08-03 12:19:47 +0100

    feat: accept svg files

--- metro.config.js
+++ metro.config.js
@@ -4,7 +4,6 @@ module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
-
   return {
     transformer: {
       babelTransformerPath: require.resolve('./asset-transformer.js'),
