a9c3965a34917461527e2f33ea86df9dffac76e4
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-08-04 10:47:31 +0100

    Revert "feat: accept svg files"
    This reverts commit d0a56d68ec1ba72efc3c593e85b6b8eeaa54e345.

--- metro.config.js
+++ metro.config.js
@@ -4,6 +4,7 @@ module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
+
   return {
     transformer: {
       babelTransformerPath: require.resolve('./asset-transformer.js'),
