7dc2d0598e6190ad181fddc4f46e40ada6c37235
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-08-04 10:48:39 +0100

    Revert "Merge branch 'chores/svg-transformer' into develop"
    This reverts commit adf939bbbac79a3a349558017be89675ddfb635c, reversing
    changes made to c61ead7940c3ef4e45e9aca5a37594873f2c1c74.

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,5 @@
-const { getDefaultConfig } = require('metro-config');
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('./asset-transformer.js'),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+module.exports = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-typescript-transformer'),
+  },
+};
