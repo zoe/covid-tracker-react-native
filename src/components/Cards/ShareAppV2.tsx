a80def9a98523e08d8e292bea32556cd69eae5d5
Somesh Selvachandran <somesh@joinzoe.com>
2020-08-04 06:31:17 +0100

    Diet Study Changes (#637)
    * Change Intro page Copy
    
    * Update copy on thank you break screen.
    
    * Move Consent Screen into middle of the flow.
    
    * Update consent page in the middle of the flow.
    
    * Update Thank You Page
    
    * Align text
    
    * Finalise copy on Thank You page
    
    * Fix json
    
    Co-authored-by: Abu Buwe <52913482+abubuwe@users.noreply.github.com>

---
+++ src/components/Cards/ShareAppV2.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Image, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+
+import i18n from '@covid/locale/i18n';
+import { shareAppV2 } from '@assets';
+
+import { isAndroid } from '../Screen';
+
+import { CommonShareProps, BaseShareAppCard, shareUrl, shareApp } from './BaseShareApp';
+
+export const ShareAppCardV2: React.FC<CommonShareProps> = (props) => {
+  const shareMessage = i18n.t('share-this-app.message');
+  const share = async () => {
+    const message = shareMessage + (isAndroid ? ' ' + shareUrl() : ''); // On Android add link to end of message
+    shareApp(message);
+  };
+  const { onSharePress = () => share() } = props;
+
+  return (
+    <TouchableWithoutFeedback onPress={onSharePress}>
+      <View style={styles.viewContainer}>
+        <Image source={shareAppV2} style={[styles.image, { aspectRatio: 1.92 }]} />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {},
+
+  viewContainer: {
+    marginVertical: 20,
+    marginHorizontal: 10,
+    alignSelf: 'center',
+    flex: 1,
+    flexDirection: 'row',
+  },
+  image: {
+    resizeMode: 'contain',
+    width: '100%',
+  },
+});
