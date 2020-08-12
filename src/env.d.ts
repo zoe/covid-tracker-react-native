2e6a338023e2cfb57c004a3177659be522cd6e34
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-08-12 19:08:10 +0100

    fix: add website site id

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,4 +2,5 @@ declare module 'react-native-dotenv' {
   export const API_URL: string;
   export const AMPLITUDE_KEY: string;
   export const WEBFLOW_KEY: string;
+  export const WEBFLOW_SITE: string;
 }
