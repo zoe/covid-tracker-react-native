781e8e50bbd435b6e1fd6feb133d2ef004873a76
Abu Buwe <abu@joinzoe.com>
2020-08-21 18:14:25 +0100

    adds new generic link tracking

--- src/utils/links.ts
+++ src/utils/links.ts
@@ -1,5 +1,8 @@
 import { Linking } from 'react-native';
 
+import Analytics, { events } from '@covid/core/Analytics';
+
 export const openExternalLink = (link: string) => {
+  Analytics.track(events.LINK_OPENED, { link });
   Linking.openURL(link);
 };
