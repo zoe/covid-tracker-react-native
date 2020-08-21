7f9d37afd01498ab4d6271faa97cd7e2589ed0c7
Somesh Selvachandran <mail@somesh.co.uk>
2020-08-21 10:33:52 +0100

    Update order

--- src/features/thank-you/ThankYouUKScreen.tsx
+++ src/features/thank-you/ThankYouUKScreen.tsx
@@ -69,6 +69,14 @@ export default class ThankYouUKScreen extends Component<RenderProps, State> {
               </View>
 
               <ExternalCallout
+                link="https://covid.joinzoe.com/your-contribution?utm_source=App"
+                calloutID="data_page_003"
+                imageSource={dataPage003}
+                aspectRatio={1.55}
+                screenName={this.props.route.name}
+              />
+
+              <ExternalCallout
                 link="https://covid.joinzoe.com/post/grant?utm_source=App"
                 calloutID="blog_009"
                 imageSource={blog009}
@@ -81,14 +89,6 @@ export default class ThankYouUKScreen extends Component<RenderProps, State> {
                 calloutID="tim_update_005"
                 imageSource={timUpdate005}
                 aspectRatio={1.178}
-                screenName={this.props.route.name}
-              />
-
-              <ExternalCallout
-                link="https://covid.joinzoe.com/your-contribution?utm_source=App"
-                calloutID="data_page_003"
-                imageSource={dataPage003}
-                aspectRatio={1.55}
                 screenName={this.props.route.name}
               />
 
