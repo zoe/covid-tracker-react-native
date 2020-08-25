c5a07362a9ef5504ca635e97e66fc460902aa10c
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-08-25 17:10:05 +0100

    feat: use Provider to inject redux store

--- App.tsx
+++ App.tsx
@@ -1,18 +1,22 @@
 import 'reflect-metadata';
 import React from 'react';
+import { Provider } from 'react-redux';
 
 import CovidApp from '@covid/CovidApp';
 import StorybookUIRoot from '@covid/storybook';
-import { Provider } from '@covid/provider/services.provider';
+import { Provider as ServiceProvider } from '@covid/provider/services.provider';
 import { container } from '@covid/provider/services';
+import store from '@covid/core/state/store';
 
 const ENABLE_STORYBOOK = false;
 
 const App: React.FC = () => {
   const Root = ENABLE_STORYBOOK ? StorybookUIRoot : CovidApp;
   return (
-    <Provider container={container}>
-      <Root />
+    <Provider store={store}>
+      <ServiceProvider container={container}>
+        <Root />
+      </ServiceProvider>
     </Provider>
   );
 };
