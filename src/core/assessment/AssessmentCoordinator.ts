d5e858adc10c06862e25d79793cbe5a0395c2e8d
Kenneth Read <kenneth@joinzoe.com>
2020-08-26 18:12:30 +0100

    unused import

--- src/core/assessment/AssessmentCoordinator.ts
+++ src/core/assessment/AssessmentCoordinator.ts
@@ -4,7 +4,7 @@ import { PatientStateType } from '@covid/core/patient/PatientState';
 import { IUserService } from '@covid/core/user/UserService';
 import { CovidTest, CovidTestType } from '@covid/core/user/dto/CovidTestContracts';
 import { ScreenParamList } from '@covid/features/ScreenParamList';
-import appCoordinator, { AppCoordinator } from '@covid/features/AppCoordinator';
+import { AppCoordinator } from '@covid/features/AppCoordinator';
 import { ILocalisationService, isSECountry, isUSCountry } from '@covid/core/localisation/LocalisationService';
 import { Services } from '@covid/provider/services.types';
 import { lazyInject } from '@covid/provider/services';
