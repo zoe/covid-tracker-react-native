d4013d04f2785dd5cb27006ab1c94b970edbbc75
cm-hvg <36766508+cm-hvg@users.noreply.github.com>
2020-09-01 17:02:29 +0100

    feat: updated progress status to be more flexible

--- src/components/ProgressStatus.tsx
+++ src/components/ProgressStatus.tsx
@@ -9,16 +9,18 @@ import { screenWidth } from './Screen';
 type ProgressProps = {
   step: number;
   maxSteps: number;
+  color?: string;
 };
 
 const ProgressStatus: React.FC<ProgressProps> = (props) => {
   const progress = (props.step * 100) / props.maxSteps;
+  const color = props.color ?? colors.purple;
   return (
     <View style={styles.progressBar}>
       <Progress.Bar
         progress={progress / 100}
         width={screenWidth - 36}
-        color={colors.purple}
+        color={color}
         unfilledColor={colors.backgroundFour}
         borderWidth={0}
         height={2}
