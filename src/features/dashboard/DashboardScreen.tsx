2b1748688a5a8afe3fa1401e560cbe2848809046
snack-0verflow <36766508+cm-hvg@users.noreply.github.com>
2020-09-08 15:42:25 +0100

    fix: remove extra spacing

--- src/features/dashboard/DashboardScreen.tsx
+++ src/features/dashboard/DashboardScreen.tsx
@@ -82,7 +82,6 @@ export const DashboardScreen: React.FC<Props> = ({ navigation, route }) => {
         <View
           style={{
             marginHorizontal: 32,
-            marginBottom: 16,
           }}>
           <SchoolNetworks networks={networks!} />
         </View>
