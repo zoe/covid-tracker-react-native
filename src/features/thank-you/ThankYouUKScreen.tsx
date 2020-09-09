14a5181d2e8848e75d391412392c079d9883bf56
Somesh Selvachandran <mail@somesh.co.uk>
2020-09-09 18:40:12 +0100

    Update blog

--- src/features/thank-you/ThankYouUKScreen.tsx
+++ src/features/thank-you/ThankYouUKScreen.tsx
@@ -4,7 +4,7 @@ import { Text } from 'native-base';
 import React, { Component } from 'react';
 import { SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 
-import { blog009, dataPage003, timUpdate007, notificationReminders } from '@assets';
+import { blog010, dataPage003, timUpdate007, notificationReminders } from '@assets';
 import { colors } from '@theme';
 import { AppRating, shouldAskForRating } from '@covid/components/AppRating';
 import { ExternalCallout } from '@covid/components/ExternalCallout';
@@ -77,9 +77,9 @@ export default class ThankYouUKScreen extends Component<RenderProps, State> {
               />
 
               <ExternalCallout
-                link="https://covid.joinzoe.com/post/grant?utm_source=App"
-                calloutID="blog_009"
-                imageSource={blog009}
+                link="https://covid.joinzoe.com/post/covid-on-the-rise?utm_source=App"
+                calloutID="blog_010"
+                imageSource={blog010}
                 aspectRatio={1.552}
                 screenName={this.props.route.name}
               />
