ec71133e93ca0b2890e76d726bac55b1494762a3
Somesh Selvachandran <somesh@joinzoe.com>
2020-09-10 21:32:05 +0100

    Add featured blog post on Dashboard (#768)

--- src/features/dashboard/DashboardScreen.tsx
+++ src/features/dashboard/DashboardScreen.tsx
@@ -13,7 +13,7 @@ import { ScreenParamList } from '@covid/features/ScreenParamList';
 import appCoordinator from '@covid/features/AppCoordinator';
 import { ExternalCallout } from '@covid/components/ExternalCallout';
 import { share } from '@covid/components/Cards/BaseShareApp';
-import { shareAppV3, schoolNetworkFeature } from '@assets';
+import { shareAppV3, schoolNetworkFeature, blog010 } from '@assets';
 import i18n from '@covid/locale/i18n';
 import { isGBCountry } from '@covid/core/localisation/LocalisationService';
 import { openWebLink } from '@covid/utils/links';
@@ -78,6 +78,18 @@ export const DashboardScreen: React.FC<Props> = ({ navigation, route }) => {
       navigation={navigation}
       compactHeader={<CompactHeader reportOnPress={onReport} />}
       expandedHeader={<Header reportOnPress={onReport} />}>
+      {isGBCountry() && (
+        <View style={styles.calloutContainer}>
+          <ExternalCallout
+            link="https://covid.joinzoe.com/post/covid-on-the-rise?utm_source=App"
+            calloutID="blog_010"
+            imageSource={blog010}
+            aspectRatio={1.552}
+            screenName={route.name}
+          />
+        </View>
+      )}
+
       {hasNetworkData && (
         <View
           style={{
