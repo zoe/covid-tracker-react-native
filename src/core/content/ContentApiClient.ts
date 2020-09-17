9ed4736f14f9da2aa6f9c5c11e8f44e000d6c438
Somesh Selvachandran <somesh@joinzoe.com>
2020-09-17 09:58:49 +0100

    CHORE: Remove calls to backkend app_text API.  (#781)
    * Remove app text
    
    * Set state after i18n triggers

--- src/core/content/ContentApiClient.ts
+++ src/core/content/ContentApiClient.ts
@@ -1,14 +1,12 @@
 import { injectable, inject } from 'inversify';
 
 import { AreaStatsResponse, StartupInfo } from '@covid/core/user/dto/UserAPIContracts';
-import { AppScreenContent } from '@covid/core/content/ScreenContentContracts';
 import { Services } from '@covid/provider/services.types';
 import { IApiClient } from '@covid/core/api/ApiClient';
 
 export interface IContentApiClient {
   getAreaStats(patientId: string): Promise<AreaStatsResponse>;
   getStartupInfo(): Promise<StartupInfo>;
-  getScreenContent(countryCode: string, languageCode: string): Promise<AppScreenContent>;
 }
 
 @injectable()
@@ -22,11 +20,5 @@ export class ContentApiClient implements IContentApiClient {
 
   getStartupInfo(): Promise<StartupInfo> {
     return this.apiClient.get<StartupInfo>('/users/startup_info/');
-  }
-
-  getScreenContent(countryCode: string, languageCode: string): Promise<AppScreenContent> {
-    return this.apiClient.get<AppScreenContent>(
-      `/text/list/?country_code=${countryCode}&language_code=${languageCode}`
-    );
   }
 }
