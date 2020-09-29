4e251cd89899a4121b6c2ef4f8268dc20e68f63e
snack-0verflow <36766508+cm-hvg@users.noreply.github.com>
2020-09-29 15:04:24 +0100

    chore: add lint / test to circle ci report

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,5 @@ module.exports = {
     './node_modules/reflect-metadata/Reflect.js',
     'node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)',
   ],
+  reporters: ['default', 'jest-junit'],
 };
