f91b44757756215fbf3f4868f1aefab5a776694c
Peter <masonptr@gmail.com>
2020-12-02 11:58:01 +0000

    remove divider line

--- src/components/Cards/EstimatedCase/TrendlineCard.tsx
+++ src/components/Cards/EstimatedCase/TrendlineCard.tsx
@@ -95,14 +95,10 @@ export const TrendlineCard: React.FC<Props> = ({ ctaOnPress = () => null, isShar
       </View>
 
       {!isSharing && (
-        <>
-          <View style={styles.divider} />
-
-          <TouchableOpacity style={styles.shareTouchable} onPress={() => navigate('Share', { sharable: 'TRENDLINE' })}>
-            <Share style={styles.shareIcon} />
-            <MutedText style={styles.shareLabel}>{i18n.t('dashboard.trendline-card.share-cta')}</MutedText>
-          </TouchableOpacity>
-        </>
+        <TouchableOpacity style={styles.shareTouchable} onPress={() => navigate('Share', { sharable: 'TRENDLINE' })}>
+          <Share style={styles.shareIcon} />
+          <MutedText style={styles.shareLabel}>{i18n.t('dashboard.trendline-card.share-cta')}</MutedText>
+        </TouchableOpacity>
       )}
     </View>
   );
