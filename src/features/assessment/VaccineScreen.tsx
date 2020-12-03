effa3bc0a4fd250fff43d9b999b27e628db6eaa7
Somesh Selvachandran <mail@somesh.co.uk>
2020-12-03 09:42:41 +0000

    Intermediate checkin

--- src/features/assessment/VaccineScreen.tsx
+++ src/features/assessment/VaccineScreen.tsx
@@ -1,16 +1,19 @@
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState } from 'react';
-import { Form } from 'native-base';
-import { Text, View } from 'react-native';
+import { Form, Text } from 'native-base';
+import { StyleSheet, View } from 'react-native';
 
 import Screen, { Header } from '@covid/components/Screen';
-import { BrandedButton, HeaderText } from '@covid/components/Text';
+import { BrandedButton, HeaderText, RegularText } from '@covid/components/Text';
 import assessmentCoordinator from '@covid/core/assessment/AssessmentCoordinator';
 import i18n from '@covid/locale/i18n';
 import { assessmentService } from '@covid/Services';
 import { SelectorButton } from '@covid/components/SelectorButton';
 import { VaccineRequest } from '@covid/core/vaccines/dto/VaccineRequest';
+import { colors } from '@theme';
+import IInfoCircle from '@assets/icons/I-InfoCircle';
+import InfoCircle from '@assets/icons/InfoCircle';
 
 import { ScreenParamList } from '../ScreenParamList';
 
@@ -41,31 +44,65 @@ export const VaccineScreen: React.FC<Props> = ({ route, navigation }) => {
 
   const currentPatient = assessmentCoordinator.assessmentData.patientData.patientState;
   return (
-    <>
+    <View style={styles.rootContainer}>
       <Screen profile={currentPatient.profile} navigation={navigation}>
         <Header>
+          <View style={{ flexDirection: 'row', flex: 1 }}>
+            <RegularText>{i18n.t('vaccines.weekly-label')}</RegularText>
+          </View>
+
           <HeaderText>{i18n.t('vaccines.title')}</HeaderText>
         </Header>
 
-        <View>
-          <Text>{i18n.t('vaccines.question-text')}</Text>
-        </View>
-
-        <Form style={{ flexGrow: 1 }}>
-          <View style={{ marginHorizontal: 16 }}>
-            <SelectorButton onPress={() => handlePress('yes')} text={i18n.t('vaccines.answer-yes')} />
-            <SelectorButton onPress={() => handlePress('no')} text={i18n.t('vaccines.answer-no')} />
+        <View style={{ marginHorizontal: 16 }}>
+          <View style={{ marginVertical: 16 }}>
+            <RegularText>{i18n.t('vaccines.question-text')}</RegularText>
           </View>
 
-          {takenVaccine === 'yes' && <Text>{i18n.t('vaccines.yes-info')}</Text>}
-          {takenVaccine === 'no' && <Text>{i18n.t('vaccines.no-info')}</Text>}
+          <Form style={{ flexGrow: 1 }}>
+            <SelectorButton onPress={() => handlePress('yes')} text={i18n.t('vaccines.answer-yes')} />
+            <SelectorButton onPress={() => handlePress('no')} text={i18n.t('vaccines.answer-no')} />
 
-          <View style={{ flex: 1 }} />
-          <BrandedButton onPress={handleSubmit} hideLoading={isSubmitting}>
-            {i18n.t('vaccines.confirm')}
-          </BrandedButton>
-        </Form>
+            {takenVaccine === 'yes' && (
+              <View style={{ marginVertical: 16, flexDirection: 'row' }}>
+                <View>
+                  <InfoCircle />
+                </View>
+                <View>
+                  <RegularText>{i18n.t('vaccines.yes-info')}</RegularText>
+                </View>
+              </View>
+            )}
+
+            {takenVaccine === 'no' && (
+              <View style={{ marginVertical: 16, flexDirection: 'row' }}>
+                <View style={{ marginRight: 8 }}>
+                  <InfoCircle />
+                </View>
+                <View>
+                  <RegularText>{i18n.t('vaccines.no-info')}</RegularText>
+                </View>
+              </View>
+            )}
+          </Form>
+        </View>
       </Screen>
-    </>
+
+      <BrandedButton style={styles.continueButton} onPress={handleSubmit} hideLoading={isSubmitting}>
+        <Text>{i18n.t('vaccines.confirm')}</Text>
+      </BrandedButton>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    backgroundColor: colors.backgroundPrimary,
+  },
+
+  continueButton: {
+    marginHorizontal: 16,
+    marginBottom: 32,
+  },
+});
