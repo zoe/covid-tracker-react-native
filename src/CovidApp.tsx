63f30479eaf81f8df36847eeb26680a7e55823bf
Peter <masonptr@gmail.com>
2020-12-14 17:25:20 +0000

    make function

--- src/CovidApp.tsx
+++ src/CovidApp.tsx
@@ -2,7 +2,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { Header, Root } from 'native-base';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { Dimensions, StatusBar } from 'react-native';
 import * as Notifications from 'expo-notifications';
 
@@ -17,48 +17,47 @@ import { MainNavigator } from '@covid/routes';
 const Stack = createStackNavigator<ScreenParamList>();
 const Drawer = createDrawerNavigator();
 
-export default class CovidApp extends Component {
-  async componentDidMount() {
+function DrawNavigator() {
+  return (
+    <Drawer.Navigator
+      drawerContent={(props) => <DrawerMenu {...props} />}
+      screenOptions={{ swipeEnabled: false }}
+      drawerStyle={{
+        width: Dimensions.get('screen').width,
+      }}>
+      <Drawer.Screen name="Main" component={MainNavigator} options={{ headerShown: false }} />
+    </Drawer.Navigator>
+  );
+}
+
+function CovidApp() {
+  useEffect(() => {
     Notifications.addNotificationResponseReceivedListener((response) => {
-      // const url = response.notification.request.content.data.url;
       Analytics.track(events.OPEN_FROM_NOTIFICATION);
     });
-  }
+  });
 
-  render() {
-    return (
-      <Root>
-        <Header style={{ display: 'none' }}>
-          <StatusBar backgroundColor={colors.white} barStyle="dark-content" />
-        </Header>
-        <NavigationContainer
-          ref={(navigatorRef) => {
-            NavigatorService.setContainer(navigatorRef);
-          }}
-          onStateChange={NavigatorService.handleStateChange}>
-          <Stack.Navigator headerMode="none" mode="modal" initialRouteName="Main">
-            <Stack.Screen name="Main" component={this.drawNavigator} />
-            <Stack.Screen
-              name="Share"
-              component={ShareScreen}
-              options={{ cardStyle: { backgroundColor: 'rgba(0,0,0,0.8)' } }}
-            />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </Root>
-    );
-  }
-
-  drawNavigator = () => {
-    return (
-      <Drawer.Navigator
-        drawerContent={(props) => <DrawerMenu {...props} />}
-        screenOptions={{ swipeEnabled: false }}
-        drawerStyle={{
-          width: Dimensions.get('screen').width,
-        }}>
-        <Drawer.Screen name="Main" component={MainNavigator} options={{ headerShown: false }} />
-      </Drawer.Navigator>
-    );
-  };
+  return (
+    <Root>
+      <Header style={{ display: 'none' }}>
+        <StatusBar backgroundColor={colors.white} barStyle="dark-content" />
+      </Header>
+      <NavigationContainer
+        ref={(navigatorRef) => {
+          NavigatorService.setContainer(navigatorRef);
+        }}
+        onStateChange={NavigatorService.handleStateChange}>
+        <Stack.Navigator headerMode="none" mode="modal" initialRouteName="Main">
+          <Stack.Screen name="Main" component={DrawNavigator} />
+          <Stack.Screen
+            name="Share"
+            component={ShareScreen}
+            options={{ cardStyle: { backgroundColor: 'rgba(0,0,0,0.8)' } }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Root>
+  );
 }
+
+export default CovidApp;
