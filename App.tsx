e01c858ff954cfb1e9c97ce889d088cd0de9ba68
Peter Mason <masonptr@gmail.com>
2020-12-14 17:44:03 +0000

    remove unwated font load (#1006)
    * remove unwated font load
    
    * remove conditional state
    
    * remove unecessary font loading
    
    * remove font loaders
    
    * remove dead code
    
    * make function

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import 'reflect-metadata';
 import React from 'react';
-import { LogBox } from 'react-native';
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components/native';
 import SplashScreen from 'react-native-splash-screen';
-import { useFonts } from 'expo-font';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import CovidApp from '@covid/CovidApp';
@@ -19,26 +17,15 @@ const ENABLE_STORYBOOK = false;
 
 const App: React.FC = () => {
   const Root = ENABLE_STORYBOOK ? StorybookUIRoot : CovidApp;
-  const [fontsLoaded] = useFonts({
-    sofiaProBlack: require('@assets/fonts/SofiaPro-Black.otf'),
-    sofiaProBold: require('@assets/fonts/SofiaPro-Bold.otf'),
-    sofiaProExtraLight: require('@assets/fonts/SofiaPro-ExtraLight.otf'),
-    sofiaProLight: require('@assets/fonts/SofiaPro-Light.otf'),
-    sofiaProMedium: require('@assets/fonts/SofiaPro-Medium.otf'),
-    sofiaProSemiBold: require('@assets/fonts/SofiaPro-SemiBold.otf'),
-    sofiaProUltraLight: require('@assets/fonts/SofiaPro-UltraLight.otf'),
-    sofiaProRegular: require('@assets/fonts/SofiaProRegular.otf'),
-  });
   SplashScreen.hide();
-  // *** //
-  // LogBox.ignoreAllLogs(true);
-  // *** //
   return (
     <Provider store={store}>
       <ThemeProvider theme={Theme}>
         <SafeAreaProvider>
           <MessagingContainer />
-          <ServiceProvider container={container}>{fontsLoaded ? <Root /> : null}</ServiceProvider>
+          <ServiceProvider container={container}>
+            <Root />
+          </ServiceProvider>
         </SafeAreaProvider>
       </ThemeProvider>
     </Provider>
