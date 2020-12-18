595b24f0177a8238717ffea9765bfe9e7c47e5cb
Peter <masonptr@gmail.com>
2020-12-18 17:30:38 +0000

    removed debug button

--- src/features/diet-scores/missing-data-text/index.tsx
+++ src/features/diet-scores/missing-data-text/index.tsx
@@ -9,7 +9,7 @@ function MissingDataText() {
   return (
     <View>
       <DietScoreHeader title="Before the pandemic" subTitle="February 2020" />
-      <Text textClass="pSmall">
+      <Text textClass="pSmallLight">
         You either have told us your diet did not change pre-pandemic (so the score should be the same as above) or we
         do not have answers from you.
       </Text>
