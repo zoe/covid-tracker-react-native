4d0282a0982cd70e2e723215446b5ba4bbc3d2f1
Peter <masonptr@gmail.com>
2021-01-04 15:55:38 +0000

    configure redux persist

--- src/core/state/store.ts
+++ src/core/state/store.ts
@@ -1,12 +1,31 @@
 import { useDispatch } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import thunk from 'redux-thunk';
 
 import rootReducer from '@covid/core/state/root';
 
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export default () => {
+//   let store = createStore(persistedReducer)
+//   let persistor = persistStore(store)
+//   return { store, persistor }
+// }
+
 const store = configureStore({
-  reducer: rootReducer,
-  devTools: true,
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 
