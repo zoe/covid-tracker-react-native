f4cce892a7991053fb135b7f4a17d4459a7e5f91
Somesh Selvachandran <mail@somesh.co.uk>
2021-01-25 18:20:48 +0000

    Restyle the error box

--- src/core/ErrorBoundary.tsx
+++ src/core/ErrorBoundary.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { View, StyleSheet, Button, Image } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import * as Sentry from 'sentry-expo';
 
 import { Header3Text, RegularBoldText, RegularText } from '@covid/components/Text';
 import { colors } from '@theme';
-import { covidIcon } from '@assets';
+import { errors } from '@assets';
 import i18n from '@covid/locale/i18n';
 
 const myErrorHandler = (error: Error, componentStack: string, eventId: string) => {
@@ -19,7 +19,7 @@ function ErrorFallback(errorData: {
   return (
     <View style={[styles.outerContainer]}>
       <View style={[styles.innerContainer]}>
-        <Image source={covidIcon} style={styles.logo} />
+        <Image source={errors} style={styles.logo} />
         <Header3Text>{i18n.t('error-boundary.something-went-wrong')}</Header3Text>
         <RegularText>{'\n' + i18n.t('error-boundary.encountered-error')}</RegularText>
         <RegularBoldText>'{errorData.error.message}'</RegularBoldText>
@@ -38,20 +38,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignContent: 'center',
+    backgroundColor: colors.backgroundPrimary,
   },
   innerContainer: {
     margin: 16,
-    backgroundColor: colors.backgroundTertiary,
-    borderRadius: 8,
-    borderWidth: 2,
+    backgroundColor: colors.backgroundPrimary,
     justifyContent: 'center',
     alignContent: 'center',
     padding: 12,
   },
   logo: {
     resizeMode: 'contain',
-    height: 70,
-    width: 70,
-    marginBottom: 8,
+    marginBottom: 32,
   },
 });
