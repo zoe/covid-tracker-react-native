c15d9c37269d06173ae54b5943d283d8b68df2af
the-kenneth <36822087+the-kenneth@users.noreply.github.com>
2021-01-25 18:32:31 +0000

    Add Sentry crash logging and global error boundary (#1058)
    * Add Sentry crash logging and global error boundary
    
    * Fis env variable name clash
    
    * Fix env variable in config
    
    * Translations
    
    * Fix pod version downgrade
    
    * Disable session tracking
    
    * Fix merge conflicts
    
    * Typo
    
    * Restyle the error box
    
    * Remove shell script from ios build process
    
    Co-authored-by: Somesh Selvachandran <mail@somesh.co.uk>
    Co-authored-by: Ben Fox <deathbycornishpastie@hotmail.com>

---
+++ src/core/ErrorBoundary.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Image, StyleSheet, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+import { Header3Text, RegularBoldText, RegularText } from '@covid/components/Text';
+import { colors } from '@theme';
+import { errors } from '@assets';
+import i18n from '@covid/locale/i18n';
+
+const myErrorHandler = (error: Error, componentStack: string, eventId: string) => {
+  Sentry.Native.captureException(error);
+};
+function ErrorFallback(errorData: {
+  error: Error;
+  componentStack: string | null;
+  eventId: string | null;
+  resetError(): void;
+}) {
+  return (
+    <View style={[styles.outerContainer]}>
+      <View style={[styles.innerContainer]}>
+        <Image source={errors} style={styles.logo} />
+        <Header3Text>{i18n.t('error-boundary.something-went-wrong')}</Header3Text>
+        <RegularText>{'\n' + i18n.t('error-boundary.encountered-error')}</RegularText>
+        <RegularBoldText>'{errorData.error.message}'</RegularBoldText>
+        <RegularText>{'\n' + i18n.t('error-boundary.notified')}</RegularText>
+      </View>
+    </View>
+  );
+}
+export const ErrorBoundary = ({ children }: { children: React.ReactNode }) => (
+  <Sentry.Native.ErrorBoundary fallback={ErrorFallback} onError={myErrorHandler}>
+    {children}
+  </Sentry.Native.ErrorBoundary>
+);
+const styles = StyleSheet.create({
+  outerContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: colors.backgroundPrimary,
+  },
+  innerContainer: {
+    margin: 16,
+    backgroundColor: colors.backgroundPrimary,
+    justifyContent: 'center',
+    alignContent: 'center',
+    padding: 12,
+  },
+  logo: {
+    resizeMode: 'contain',
+    marginBottom: 32,
+  },
+});
