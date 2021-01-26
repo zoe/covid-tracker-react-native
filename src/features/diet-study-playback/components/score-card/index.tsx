517d89f7aa5f0edd29fe4cb5c49023cea41a2ff4
Somesh Selvachandran <mail@somesh.co.uk>
2021-01-26 20:44:10 +0000

    Refactor

--- src/features/diet-study-playback/components/score-card/index.tsx
+++ src/features/diet-study-playback/components/score-card/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement, ReactNode } from 'react';
-import { Animated, View } from 'react-native';
+import { Animated } from 'react-native';
 
 type TDirection = 'UP' | 'DOWN';
 
