aaaf21036834a2f053d09650f17fbbaf2297a6fb
Peter <masonptr@gmail.com>
2021-02-02 09:44:59 +0000

    wip

---
+++ src/features/mental-health/screens/start.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function MentalHealthStart() {
+  return (
+    <View>
+      <Text>The end</Text>
+    </View>
+  );
+}
+
+export default MentalHealthStart;
