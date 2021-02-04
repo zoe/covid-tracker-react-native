d09fd80181ac54f5b48f984e47e4bd720add2377
Peter <masonptr@gmail.com>
2021-02-04 14:23:36 +0000

    wip

--- src/components/layouts/safe-layout/index.tsx
+++ src/components/layouts/safe-layout/index.tsx
@@ -1,15 +1,18 @@
 import React, { ReactNode } from 'react';
 import { SafeAreaView, StyleSheet, View } from 'react-native';
 
+import { useTheme } from '@covid/themes';
+
 interface IProps {
   children: ReactNode;
   withGutter?: boolean;
 }
 
 function SafeLayout({ children, withGutter = true }: IProps) {
+  const theme = useTheme();
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container, { paddingHorizontal: withGutter ? 16 : 0 }]}>{children}</View>
+      <View style={[styles.container, { paddingHorizontal: withGutter ? theme.grid.gutter : 0 }]}>{children}</View>
     </SafeAreaView>
   );
 }
