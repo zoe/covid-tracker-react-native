948176e28e3d91691b8886d19afc0fc121f6a8ed
Ben Fox <deathbycornishpastie@hotmail.com>
2021-02-09 18:39:06 +0000

    Updated homepage vaccine share to use current share system

---
+++ src/components/Cards/ShareVaccineCard.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { shareVaccineBanner, shareVaccine } from '@assets';
+
+import { ExternalCallout } from '../ExternalCallout';
+
+interface Props {
+  screenName: string;
+  isSharing?: boolean;
+}
+
+export const ShareVaccineCard: React.FC<Props> = ({ screenName, isSharing = false }) => {
+  const { navigate } = useNavigation();
+
+  return (
+    <>
+      {!isSharing && (
+        <ExternalCallout
+          calloutID="shareVaccineBanner"
+          imageSource={shareVaccineBanner}
+          aspectRatio={311 / 135}
+          screenName={screenName}
+          postClicked={() => navigate('Share', { sharable: 'VACCINES', hideLabel: true })}
+        />
+      )}
+
+      {isSharing && (
+        <ExternalCallout
+          calloutID="shareVaccine"
+          imageSource={shareVaccine}
+          aspectRatio={600 / 500}
+          screenName={screenName}
+          postClicked={() => navigate('Share', { sharable: 'VACCINES', hideLabel: true })}
+        />
+      )}
+    </>
+  );
+};
