eb7808e3fc4d8d49ed47a0954b3f24d50b87efc1
Peter <masonptr@gmail.com>
2021-02-10 16:58:39 +0000

    move data out of view component

--- src/features/mental-health/screens/MentalHealthHistory.tsx
+++ src/features/mental-health/screens/MentalHealthHistory.tsx
@@ -14,102 +14,13 @@ import {
   THasDiagnosis,
 } from '@covid/core/state/mental-health';
 
-type TQuestion = {
-  key: string;
-  value: TMentalHealthCondition;
-};
-
-type TOptions = {
-  label: string;
-  value: THasDiagnosis;
-};
+import { TQuestion, questions, initialOptions } from '../data';
 
 function MentalHealthHistory() {
   const MentalHealthHistory = useSelector(selectMentalHealthHistory);
   const [canSubmit, setCanSubmit] = useState(false);
-
   const dispatch = useDispatch();
   const { grid } = useTheme();
-  const initialOptions: TOptions[] = [
-    { label: 'Yes', value: 'YES' },
-    { label: 'No', value: 'NO' },
-    { label: 'Prefer not to say', value: 'DECLINE_TO_SAY' },
-  ];
-
-  const questions: TQuestion[] = [
-    {
-      key: 'Generalised anxiety disorder (GAD)',
-      value: 'GAD',
-    },
-    {
-      key: 'Panic disorder',
-      value: 'PANIC_DISORDER',
-    },
-    {
-      key: 'Specific phobias',
-      value: 'SPECIFIC_PHOBIAS',
-    },
-    {
-      key: 'Obsessive compulsive disorder (OCD)',
-      value: 'OCD',
-    },
-    {
-      key: 'Post-traumatic stress disorder (PTSD) ',
-      value: 'PTSD',
-    },
-    {
-      key: 'Social anxiety disorder',
-      value: 'SOCIAL_ANXIETY_DISORDER',
-    },
-    {
-      key: 'Agoraphobia',
-      value: 'AGORAPHOBIA',
-    },
-    {
-      key: 'Depression',
-      value: 'DEPRESSION',
-    },
-    {
-      key: 'Attention deficit or attention deficit and hyperactivity disorder (ADD/ADHD) ',
-      value: 'ADD_ADHD',
-    },
-    {
-      key: "Autism, Asperger's or autistic spectrum disorder ",
-      value: 'AUTISTIC_SPECTRUM_DISORDER',
-    },
-    {
-      key: 'Eating disorder (e.g. bulimia nervosa; anorexia nervosa; psychological over-eating or binge-eating',
-      value: 'EATING_DISORDER',
-    },
-    {
-      key: 'A personality disorder ',
-      value: 'PERSONALITY_DISORDER',
-    },
-    {
-      key: 'Mania, hypomania, bipolar or manic depression',
-      value: 'MANIA',
-    },
-    {
-      key: 'Schizophrenia',
-      value: 'SCHIZOPHRENIA',
-    },
-    {
-      key: 'Substance use disorder',
-      value: 'SUBSTANCE_USE_DISORDER',
-    },
-    {
-      key: 'Any other type of psychosis or psychotic illness',
-      value: 'TYPE_OF_PSYCHOSIS',
-    },
-    {
-      key: 'Other',
-      value: 'OTHER',
-    },
-    {
-      key: 'Prefer not to say',
-      value: 'DECLINE_TO_SAY',
-    },
-  ];
 
   const handleSetHasHistoryDiagnosis = (value: THasDiagnosis) => {
     dispatch(setHasHistoryDiagnosis(value));
@@ -141,6 +52,18 @@ function MentalHealthHistory() {
       </View>
     );
   };
+
+  useEffect(() => {
+    if (MentalHealthHistory.hasDiagnosis === 'NO' || MentalHealthHistory.hasDiagnosis === 'DECLINE_TO_SAY') {
+      setCanSubmit(true);
+      return;
+    }
+    if (MentalHealthHistory.hasDiagnosis === 'YES' && MentalHealthHistory.conditions.length) {
+      setCanSubmit(true);
+      return;
+    }
+    setCanSubmit(false);
+  }, [MentalHealthHistory]);
 
   return (
     <BasicPage
