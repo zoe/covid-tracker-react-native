5efd586939016f0421e72d79456c63f2151efcba
Ben Fox <deathbycornishpastie@hotmail.com>
2021-02-17 17:20:22 +0000

    Deleted redundant MentalHealthResponse

--- src/features/mental-health/MentalHealthApiClient.ts
+++ src/features/mental-health/MentalHealthApiClient.ts
@@ -3,35 +3,34 @@ import { injectable, inject } from 'inversify';
 import { IApiClient } from '@covid/core/api/ApiClient';
 import { Services } from '@covid/provider/services.types';
 
-import { TMentalHealthResponse } from './MentalHealthResponse';
 import { MentalHealthInfosRequest } from './MentalHealthInfosRequest';
 
 const API_URL = '/mental_health/';
 
 export interface IMentalHealthApiClient {
-  get(id: any): Promise<TMentalHealthResponse[]>;
-  add(patientId: string, assessment: MentalHealthInfosRequest): Promise<TMentalHealthResponse>;
-  update(assessmentId: string, assessment: MentalHealthInfosRequest): Promise<TMentalHealthResponse>;
+  get(id: any): Promise<MentalHealthInfosRequest[]>;
+  add(patientId: string, assessment: MentalHealthInfosRequest): Promise<MentalHealthInfosRequest>;
+  update(assessmentId: string, assessment: MentalHealthInfosRequest): Promise<MentalHealthInfosRequest>;
 }
 
 @injectable()
 export class MentalHealthApiClient implements IMentalHealthApiClient {
   constructor(@inject(Services.Api) private apiClient: IApiClient) {}
 
-  get(): Promise<TMentalHealthResponse[]> {
-    return this.apiClient.get<TMentalHealthResponse[]>(API_URL);
+  get(): Promise<MentalHealthInfosRequest[]> {
+    return this.apiClient.get<MentalHealthInfosRequest[]>(API_URL);
   }
 
-  add(patientId: string, mentalHealth: MentalHealthInfosRequest): Promise<TMentalHealthResponse> {
+  add(patientId: string, mentalHealth: MentalHealthInfosRequest): Promise<MentalHealthInfosRequest> {
     mentalHealth = {
       patient: patientId,
       ...mentalHealth,
     };
-    return this.apiClient.post<MentalHealthInfosRequest, TMentalHealthResponse>(API_URL, mentalHealth);
+    return this.apiClient.post<MentalHealthInfosRequest, MentalHealthInfosRequest>(API_URL, mentalHealth);
   }
 
-  update(mentalHealthId: string, mentalHealth: MentalHealthInfosRequest): Promise<TMentalHealthResponse> {
+  update(mentalHealthId: string, mentalHealth: MentalHealthInfosRequest): Promise<MentalHealthInfosRequest> {
     const url = `${API_URL}${mentalHealthId}/`;
-    return this.apiClient.patch<MentalHealthInfosRequest, TMentalHealthResponse>(url, mentalHealth);
+    return this.apiClient.patch<MentalHealthInfosRequest, MentalHealthInfosRequest>(url, mentalHealth);
   }
 }
