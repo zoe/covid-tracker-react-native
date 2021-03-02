96464ea006caa9bb2abf7d9d14cefed9b9824a13
Ben Fox <deathbycornishpastie@hotmail.com>
2021-03-02 17:56:20 +0000

    Upgrade and reinstate component tests

--- src/components/BrandedButton/__tests__/BrandedButton.test.tsx
+++ src/components/BrandedButton/__tests__/BrandedButton.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { fireEvent, queryByTestId, render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 
 import { BrandedButton } from '..';
 
@@ -11,51 +11,55 @@ describe('BrandedButton tests', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  // it('Handles press events', () => {
-  //   const onPress = jest.fn();
-  //   const { getByTestId } = render(<BrandedButton onPress={onPress}>This is a branded button</BrandedButton>);
-  //   expect(onPress).toHaveBeenCalledTimes(0);
-  //   fireEvent.press(getByTestId('buttonTestID'));
-  //   expect(onPress).toHaveBeenCalledTimes(1);
-  // });
+  it('Handles press events', () => {
+    const onPress = jest.fn();
+    const { getByTestId } = render(<BrandedButton onPress={onPress}>This is a branded button</BrandedButton>);
+    expect(onPress).toHaveBeenCalledTimes(0);
+    const button = getByTestId('buttonTestID');
+    fireEvent.press(button);
+    expect(onPress).toHaveBeenCalledTimes(1);
+  });
 
-  // it('Does not fire press events when disabled', () => {
-  //   const onPress = jest.fn();
-  //   const { getByTestId } = render(
-  //     <BrandedButton onPress={onPress} enable={false}>
-  //       This is a branded button
-  //     </BrandedButton>
-  //   );
-  //   expect(onPress).toHaveBeenCalledTimes(0);
-  //   fireEvent.press(getByTestId('buttonTestID'));
-  //   expect(onPress).toHaveBeenCalledTimes(0);
-  // });
-  // it('Handles style props being passed', () => {
-  //   const onPress = jest.fn();
-  //   const style = { background: 'red' };
-  //   const { getByTestId } = render(
-  //     <BrandedButton style={style} onPress={onPress} enable={false}>
-  //       This is a branded button
-  //     </BrandedButton>
-  //   );
-  //   expect(getByTestId('buttonTestID').props.style.background).toBe('red');
-  // });
+  it('Does not fire press events when disabled', () => {
+    const onPress = jest.fn();
+    const { getByTestId } = render(
+      <BrandedButton onPress={onPress} enable={false}>
+        This is a branded button
+      </BrandedButton>
+    );
+    expect(onPress).toHaveBeenCalledTimes(0);
+    fireEvent.press(getByTestId('buttonTestID'));
+    expect(onPress).toHaveBeenCalledTimes(0);
+  });
+  it('Handles style props being passed', () => {
+    const onPress = jest.fn();
+    const style = { background: 'red' };
+    const { getByTestId } = render(
+      <BrandedButton style={style} onPress={onPress} enable={false}>
+        This is a branded button
+      </BrandedButton>
+    );
+    expect(getByTestId('buttonTestID').props.style.background).toBe('red');
+  });
   // it('Shows loading if disabled', () => {
   //   const onPress = jest.fn();
-  //   const { container } = render(
+  //   const { queryByTestId } = render(
   //     <BrandedButton onPress={onPress} enable={false}>
   //       This is a branded button
   //     </BrandedButton>
   //   );
-  //   expect(queryByTestId(container, 'activityIndicator')).toBeTruthy();
+  //   const activityIndicator = queryByTestId('activityIndicator')
+  //   console.log('activityIndicator ',activityIndicator)
+  //   expect(activityIndicator).toBeTruthy();
   // });
   // it('Can hide loading when disabled', () => {
   //   const onPress = jest.fn();
-  //   const { container } = render(
+  //   const { queryByTestId } = render(
+
   //     <BrandedButton onPress={onPress} enable={false} hideLoading>
   //       This is a branded button
   //     </BrandedButton>
   //   );
-  //   expect(queryByTestId(container, 'activityIndicator')).toBeNull();
+  //   expect(queryByTestId('activityIndicator')).toBeNull();
   // });
 });
