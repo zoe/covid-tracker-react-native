c2153da06efa331713d1bf9dba98c8d2988a0ed0
Somesh Selvachandran <somesh@joinzoe.com>
2021-03-30 22:22:24 +0100

    Remove centeredview (only used in storybook)

--- src/storybook/decorator.tsx
+++ src/storybook/decorator.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { View } from 'react-native';
 
-import { CenteredView } from '@covid/components/CenteredView';
 import { colors } from '@theme';
 
 export const PaddingView = (fn: () => React.ReactNode) => <View style={{ marginHorizontal: 16 }}>{fn()}</View>;
 
-export const CenterView = (fn: () => React.ReactNode) => <CenteredView>{fn()}</CenteredView>;
+export const CenterView = (fn: () => React.ReactNode) => (
+  <View style={{ flex: 1, justifyContent: 'center' }}>{fn()}</View>
+);
 
 export const DarkBackground = (fn: () => React.ReactNode) => (
   <View style={{ flex: 1, backgroundColor: colors.predict }}>{fn()}</View>
