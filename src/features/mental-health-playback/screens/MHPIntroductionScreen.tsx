64c6f102794e38ce98798e9275bcd0fa350e095d
Aike van den Brink <aikevandenbrink@gmail.com>
2021-05-07 09:11:53 +0100

    Temp commit to merge with develop.

--- src/features/mental-health-playback/screens/MHPIntroductionScreen.tsx
+++ src/features/mental-health-playback/screens/MHPIntroductionScreen.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 
 import NavigatorService from '@covid/NavigatorService';
 import { BasicPage } from '@covid/components';
+import i18n from '@covid/locale/i18n';
+import Illust from '@assets/illust.svg';
 
 export default () => (
   <BasicPage
     active
-    footerTitle="Introduction"
+    hasStickyHeader
+    footerTitle={i18n.t('mental-health-playback.introduction.button')}
     onPress={() => NavigatorService.navigate('MentalHealthPlaybackGeneral')}
-  />
+  >
+    <Illust />
+  </BasicPage>
 );
