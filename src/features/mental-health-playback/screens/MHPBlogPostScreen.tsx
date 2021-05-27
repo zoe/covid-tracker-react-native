439dcdf9c14ed591898e4c89b78a813e0967f209
Aike van den Brink <aikevandenbrink@gmail.com>
2021-05-27 11:24:31 +0100

    Delay showing the online blogpost after loading is finished.

--- src/features/mental-health-playback/screens/MHPBlogPostScreen.tsx
+++ src/features/mental-health-playback/screens/MHPBlogPostScreen.tsx
@@ -1,9 +1,10 @@
 import { BasicPage } from '@covid/components';
+import i18n from '@covid/locale/i18n';
 import NavigatorService from '@covid/NavigatorService';
-import { styling } from '@covid/themes';
-import React, { useRef } from 'react';
-import { Linking } from 'react-native';
-import WebView from 'react-native-webview';
+import { colors, styling } from '@covid/themes';
+import React, { useRef, useState } from 'react';
+import { ActivityIndicator, Linking, StyleSheet, View } from 'react-native';
+import WebView, { WebViewNavigation } from 'react-native-webview';
 import UrlParse from 'url-parse';
 
 const js = `
@@ -28,33 +29,65 @@ const url = new UrlParse(uri);
 const source = { uri };
 
 export default function MHPBlogPostScreen() {
+  const [loaded, setLoaded] = useState(false);
   const webView = useRef<WebView>(null);
+
+  function onLoadEnd() {
+    webView.current?.injectJavaScript(js);
+    if (!loaded) {
+      setTimeout(() => setLoaded(true), 250);
+    }
+  }
+
+  function onNavigationStateChange(navState: WebViewNavigation) {
+    try {
+      const navUrl = new UrlParse(navState.url);
+      if (navUrl.hostname !== url.hostname || navUrl.pathname !== url.pathname) {
+        webView.current?.stopLoading();
+        Linking.openURL(navState.url);
+      }
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.warn(error);
+    }
+  }
+
   return (
     <BasicPage
       active
-      footerTitle="Close"
+      footerTitle={i18n.t('continue')}
       onPress={() => NavigatorService.navigate('MentalHealthPlaybackRating')}
       style={styling.backgroundWhite}
     >
       <WebView
         injectedJavaScript={js}
         injectedJavaScriptBeforeContentLoaded={js}
-        onLoadEnd={() => webView.current?.injectJavaScript(js)}
-        onNavigationStateChange={(navState) => {
-          try {
-            const navUrl = new UrlParse(navState.url);
-            if (navUrl.hostname !== url.hostname || navUrl.pathname !== url.pathname) {
-              webView.current?.stopLoading();
-              Linking.openURL(navState.url);
-            }
-          } catch (error) {
-            // eslint-disable-next-line no-console
-            console.warn(error);
-          }
-        }}
+        onLoad={() => webView.current?.injectJavaScript(js)}
+        onLoadEnd={onLoadEnd}
+        onNavigationStateChange={onNavigationStateChange}
         ref={webView}
         source={source}
-      />
+      >
+        {!loaded ? (
+          <View style={styles.view}>
+            <ActivityIndicator color={colors.coral.main.bgColor} size="large" />
+          </View>
+        ) : null}
+      </WebView>
     </BasicPage>
   );
 }
+
+const styles = StyleSheet.create({
+  view: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    bottom: 0,
+    justifyContent: 'center',
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+    zIndex: 1,
+  },
+});
