fd055d97526eef1f8a784cbe092614e7ada01b1e
Aike van den Brink <aikevandenbrink@gmail.com>
2021-05-28 12:40:44 +0100

    Add a key property to the countries rendering.

--- src/features/CountrySelectScreen.tsx
+++ src/features/CountrySelectScreen.tsx
@@ -61,6 +61,7 @@ export function CountrySelectScreen(props: IProps) {
         <View style={styles.flagRow}>
           {countries.map((country, index) => (
             <TouchableOpacity
+              key={`country-${country}`}
               onPress={() => selectCountry(country.code)}
               style={index !== 0 ? [styling.flex, styling.marginLeft] : styling.flex}
             >
