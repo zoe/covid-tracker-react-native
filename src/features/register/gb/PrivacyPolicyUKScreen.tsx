d310caae68f322fd4585cc854231687f7c309b2c
Somesh Selvachandran <somesh@joinzoe.com>
2021-06-23 18:05:26 +0100

    Update UK Privacy policy

--- src/features/register/gb/PrivacyPolicyUKScreen.tsx
+++ src/features/register/gb/PrivacyPolicyUKScreen.tsx
@@ -24,9 +24,11 @@ export class PrivacyPolicyUKScreen extends React.Component<PropsType, object> {
           <RegularBoldText>Your consent{'\n'}</RegularBoldText>
           <RegularBoldText>European General Data Protection Regulation{'\n'}</RegularBoldText>
           <RegularText>
-            Because the work Zoe Global Limited does takes place in the UK, the European Union’s “General Data
-            Protection Regulation” (GDPR) applies to our processing of your personal data, even if you do not live in
-            Europe.
+            This page explains how we process information about individual people, known as “personal data”, that we
+            collect through our COVID monitoring app. If you are using the app and entering your own personal data, then
+            this applies to you. We may also be processing your personal data if someone else – a family member or
+            parent or guardian or member of your household – has entered it on your behalf, see the section headed
+            “recording information for others” for more information about this.
             {'\n\n'}
             We process two kinds of information about you:
             {'\n'}
@@ -36,93 +38,104 @@ export class PrivacyPolicyUKScreen extends React.Component<PropsType, object> {
 
           <RegularText>
             This is information about you, your health and your symptoms if unwell. It includes:
-            {'\n\n'}- Information about your health (including your body temperature, height and weight){'\n'}-
-            Information about pre-existing conditions{'\n'}- Information about your symptoms{'\n'}- Your COVID-19 test
-            status{'\n'}- Details of any treatment you have received{'\n'}- General information about you such as your
-            sex at birth, your year of birth and your location (including postcode){'\n'}- Whether you are a health
-            worker coming into contact with patients{'\n'}- Whether you are a member of the UK twins study{'\n'}
-            {'\n\n'}
-            We may also ask other questions from time to time, such as:
-            {'\n\n'}- Information about your diet{'\n'}- What you do if and when you go out, such as where you go and
-            whether you wear a mask or other protection{'\n'}
-            {'\n\n'}
-            We process this data in order that:
-            {'\n\n'}- We can better understand symptoms of COVID-19{'\n'}- We can follow the spread of COVID-19, for
-            example so that we can identify hotspots{'\n'}- We can identify the exposure of healthcare workers to
-            COVID-19{'\n'}- We can advance scientific research into the links between patient's health and their
-            response to infection by COVID-19{'\n'}- In the future we may use this data to help the NHS support sick
-            individuals
-            {'\n\n'}
-            Our legal basis for processing it is that you consented to our doing so. Because of the tight regulatory
-            requirements placed on us, we need your consent to process data about your health, which means that if you
-            do not consent (or withdraw your consent), we cannot allow you to use the app. This is not meant unkindly,
-            we are simply not able to provide you with the service without your consent.
-            {'\n\n'}
-            We share this data with people doing health research, for example, people working in:
-            {'\n\n'}- Hospitals{'\n'}- NHS.{'\n'}- Universities.{'\n'}- Health charities.{'\n'}- Other research
-            institutions.
-            {'\n\n'}A full list of institutions we have shared data with can be found at the bottom of this page. An
-            anonymous code is used to replace your personal details when we share this with researchers outside the NHS
-            or King's College London.
-            {'\n\n'}
-            Before sharing any of your data with researchers outside of the UK, we will remove your name, phone number,
-            email address and the last 3 digits of your post code to protect your privacy.
-            {'\n\n'}
-            Because of the nature of the research we carry out, we are unable to set any particular time limit on the
-            storage of your sensitive personal data, but we will keep it under regular review and ensure that it is not
-            kept longer than is necessary.
-            {'\n\n'}
-            If you wish us to stop processing your sensitive personal data, you may withdraw your consent at any time by
-            emailing us at leavecovidtracking@joinzoe.com. When you withdraw your consent, we will delete all sensitive
-            personal data we hold about you.
-            {'\n'}
+            {'\n\n'}- Information about your health (including your height and weight) and any pre-existing conditions
+            you may have.{'\n'}- Information about any symptoms you may have (including body temperature).{'\n'}-Your
+            COVID-19 test status.{'\n'}- Details of any treatment you have received for COVID.{'\n'}- General
+            information about you such as your sex at birth, your year of birth and your address.{'\n'}- Whether you are
+            a health worker coming into contact with patients.{'\n'}- Whether you are a member of the UK twins study.
+            {'\n'}- Whether you have received a COVID-19 vaccine and supporting information relating to your vaccine
+            (including dose dates, type of vaccine, batch number etc.){'\n'}- Information about any adverse effects you
+            may experience after a COVID-19 vaccine.{'\n\n'}We may also ask other questions from time to time, such as:
+            {'\n\n'}- Information about your diet.{'\n'}- Information about your mental health.{'\n'}- What you do if
+            and when you go out, such as where you go and whether you wear a mask or other protection.{'\n'}- Your
+            feelings about receiving a COVID-19 vaccine.{'\n\n'}We process this data in order that:{'\n\n'}- We can
+            better identify and understand symptoms of COVID-19.{'\n'}- We can follow the spread of COVID-19, for
+            example so that we can identify hotspots.{'\n'}- We can provide an early detection system for diseases such
+            as COVID-19 (or new variants of it){'\n'}- We can identify the exposure of healthcare workers to COVID-19.
+            {'\n'}- We can advance scientific research into the links between patients’ health and their response to
+            infection by COVID-19.{'\n'}- We can track the uptake of COVID-19 vaccines and advance scientific research
+            into the impact and effects of vaccination programmes.{'\n'}-More generally, in helping to combat the spread
+            of COVID-19 and improve the treatment and prophylaxis of it (including via patient identification for
+            treatment and vaccine trials).{'\n\n'}Our legal basis for processing it is that you consented to our doing
+            so. Because of the tight regulatory requirements placed on us, we need your consent to process data about
+            your health, which means that if you do not consent (or withdraw your consent), we cannot allow you to use
+            the app. This is not meant unkindly, we are simply not able to provide you with the service without your
+            consent.{'\n\n'}We share this data with people doing health research, for example, people working in:
+          </RegularText>
+
+          <BulletedTextBlock
+            text={['Hospitals', 'NHS', 'Universities', 'Health charities', 'Other research institutions']}
+          />
+
+          <RegularText>
+            A full list of institutions we have shared data with can be found at the bottom of this page. An anonymous
+            code is used to replace your personal details when we share this with researchers outside the NHS or King's
+            College London.{'\n\n'}Before sharing any of your data with researchers outside of the UK, we will remove
+            your name, phone number, email address and the last 3 digits of your post code to protect your privacy.
+            {'\n\n'}Because of the nature of the research we carry out, we are unable to set any particular time limit
+            on the storage of your sensitive personal data, but we will keep it under regular review and ensure that it
+            is not kept longer than is necessary.{'\n\n'}If you wish us to stop processing your sensitive personal data,
+            you may withdraw your consent at any time by emailing us at leavecovidtracking@joinzoe.com. When you
+            withdraw your consent, we will delete all sensitive personal data we hold about you.
           </RegularText>
 
           <RegularBoldText>Other personal data{'\n'}</RegularBoldText>
 
           <RegularText>
             We also collect contact information and other information from your device including:
-            {'\n\n'}- Your name (optional){'\n'}- Email address (optional){'\n'}- Phone number (optional){'\n'}- A user
-            name and password{'\n'}- IP address{'\n'}- Device ID{'\n'}
+            {'\n\n'}- your name (optional){'\n'}- email address (optional){'\n'}- phone number (optional){'\n'}- a user
+            name and password{'\n'}- IP address{'\n'}- device ID{'\n\n'}
+            We may use your email address, in order to send you emails for the following purposes:{'\n\n'}- Sending you
+            regular updates about the progress of the study from Professor Tim Spector of King’s College London.{'\n'}-
+            Occasionally, providing you with information regarding COVID-19 research and/or research articles.{'\n'}-
+            Occasionally, inviting you to participate in further COVID-19 research studies.{'\n'}- Offering you the
+            opportunity to take a COVID-19 test (for example if you have said you were unwell).{'\n'}- Fundraising to
+            support the COVID-19 symptom study or the COVID-19 related health research.{'\n'}- Asking you for feedback
+            on the app or conducting other forms of survey.{'\n'}- Keeping in touch with you about the app and its
+            performance as well as about new versions of the app or similar apps we may develop.{'\n\n'}If you do not
+            wish to receive emails from us regarding this information, then you can opt out by clicking the opt out link
+            contained in our email. We do not sell your contact information to third parties.{'\n\n'}As well as your
+            email, we use this information for:{'\n\n'}- Improving your user experience through the use of cookies.
+            {'\n'}- Identifying faults or other problems connected with the app.
           </RegularText>
 
           <RegularText>
-            We use this information for the following purposes:
-            {'\n\n'}- Asking you for feedback on the app or conducting other forms of survey.{'\n'}- Keeping in touch
-            with you about the app and its performance.{'\n'}- Sending you information about new versions of the app or
-            similar apps we may have in the future.{'\n'}- Identifying faults or other problems connected with the app
-            {'\n\n'}
-            We will not send any emails not meant individually for you (for example marketing emails) if you do not want
-            us to do so. Every such email will include a link you can click to opt-out from receiving them. We will not
-            sell your contact information to third-parties.
-            {'\n\n'}
             Our legal basis for processing this information is our legitimate interest in developing, marketing and
-            running the app.
-            {'\n\n'}
-            We keep your contact information for 6 years after the last communication with us, or the last use of the
-            app, for liability purposes, then we delete it.
-            {'\n'}
+            running the app and providing you with information as part of your experience of using the app.{'\n\n'}We
+            keep your contact information for 6 years after the last communication with us, or the last use of the app,
+            for liability purposes, then we delete it.
+          </RegularText>
+
+          <RegularBoldText>Cookies{'\n'}</RegularBoldText>
+          <RegularText>
+            Our use of cookies is a little more complicated, so we have written a detailed Cookie Policy, explaining
+            what cookies are and explaining in detail how we process different kinds of cookies.{'\n'}
           </RegularText>
 
           <RegularBoldText>Recording information for others{'\n'}</RegularBoldText>
           <RegularText>
-            The app also allows you to input information about other people in addition to your own by making a separate
-            profile for them. If the other person is able to understand the concept of consent, for example if they are
-            a mentally competent adult or mature child, then you must only do this if they have given their consent.
+            The app also allows users to input information about other people in addition to their own by making a
+            separate profile for them. If you do this and the other person is able to understand the concept of consent,
+            for example if they are a mentally competent adult or mature child, then you must only do this if they have
+            given their consent.
             {'\n\n'}
             Younger children may not be mature enough to give consent, but they may be able to understand what you are
             doing. If so, you should explain to them what you are doing and what may happen to information about them to
             the extent they are capable of understanding. You should also try to take into account their views, even if
             you make the ultimate decision. We trust you to know your child and to do what is appropriate given their
-            level of maturity.{'\n'}
+            level of maturity. These processors include:
           </RegularText>
 
           <RegularBoldText>School Attendance{'\n'}</RegularBoldText>
           <RegularText>
-            If your child is attending school, you may optionally tell us about their school, their bubble and other
-            things about their attendance. We use this information in the same way we use other sensitive personal data,
-            but in addition we may (where it would not identify any individual) use it to alert you to an infection in
-            your child’s bubble and to help the school plan for any impact of COVID-19.{'\n'}
+            If you are reporting for your child and they are attending school, you may optionally tell us about the
+            school they attend, which bubble your child belongs to in addition to the other sensitive personal data
+            collected by the app. We use this information in the same way we use other sensitive personal data, but in
+            addition we may process this data in order to:
+            {'\n\n'}- Give schools insight into the health of their students, allowing faster decision making and better
+            protection for children.{'\n'}- Reassure parents that they are their school are making informed choices
+            about their child’s safety and education.{'\n'}- Increase protection for children.{'\n'}- Collect important
+            information on the impact of COVID-19 on children for the purposes of wider scientific research.
           </RegularText>
 
           <RegularBoldText>Third party processors for both kinds of information{'\n'}</RegularBoldText>
@@ -130,10 +143,10 @@ export class PrivacyPolicyUKScreen extends React.Component<PropsType, object> {
             We use third parties to process some of your personal data on our behalf. When we allow them access to your
             data, we do not permit them to use it for their own purposes. We have in place with each processor, a
             contract that requires them only to process the data on our instructions and to take proper care in using
-            it. They are not permitted to keep the data after our relationship with them has ended.
-            {'\n\n'}
-            These processors include:
+            it. They are not permitted to keep the data after our relationship with them has ended.{'\n\n'}These
+            processors include:
           </RegularText>
+
           <BulletedTextBlock
             text={[
               'Google Cloud Platform',
@@ -154,7 +167,11 @@ export class PrivacyPolicyUKScreen extends React.Component<PropsType, object> {
 
           <RegularBoldText>Your rights{'\n'}</RegularBoldText>
           <RegularText>
-            Under the{' '}
+            Because the work Zoe Global Limited does takes place in the UK, the European Union’s “General Data
+            Protection Regulation” (GDPR) applies to our processing of your personal data, even if you do not live in
+            Europe. From 1 January 2021, the GDPR will be replaced, for most purposes, by an equivalent regulation
+            specific to the UK. This should make no difference to the rights that you have, even if you do not live in
+            the UK.{'\n\n'}Under the{' '}
             <ClickableText
               onPress={() =>
                 openWebLink('http://eur-lex.europa.eu/legal-content/EN/TXT/PDF/?uri=CELEX:32016R0679&from=EN')
@@ -227,7 +244,12 @@ export class PrivacyPolicyUKScreen extends React.Component<PropsType, object> {
             Berkeley University{'\n'}
             Nottingham University{'\n'}
             University of Trento{'\n'}
-            Lund University
+            Lund University{'\n'}
+            University of Hawaii{'\n'}
+            University of South California{'\n'}
+            The University of Texas MD Anderson Cancer Center{'\n'}
+            Northshore University HealthSystem{'\n'}
+            City University of New York
           </RegularText>
         </ScrollView>
 
