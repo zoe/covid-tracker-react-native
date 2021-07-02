4c24bed2536042172ae76af6af4be15025ef0038
Julia Tan <juliatan@users.noreply.github.com>
2021-07-02 09:42:19 +0100

    Move copy to translation files and use ReconsentScreen wrapper (#1436)

--- src/features/reconsent/screens/ReconsentDiseasePreferencesScreen.tsx
+++ src/features/reconsent/screens/ReconsentDiseasePreferencesScreen.tsx
@@ -1,15 +1,15 @@
 import { Brain } from '@assets/icons/svgIcons';
-import { SafeLayout, Text } from '@covid/components';
+import { Text } from '@covid/components';
 import DiseaseCard from '@covid/features/reconsent/components/DiseaseCard';
 import InfoBox from '@covid/features/reconsent/components/InfoBox';
-import ReconsentFooter from '@covid/features/reconsent/components/ReconsentFooter';
 import ReconsentHeader from '@covid/features/reconsent/components/ReconsentHeader';
+import ReconsentScreen from '@covid/features/reconsent/components/ReconsentScreen';
 import i18n from '@covid/locale/i18n';
 import NavigatorService from '@covid/NavigatorService';
-import { grid, styling, useTheme } from '@covid/themes';
+import { grid } from '@covid/themes';
 import { colors } from '@theme';
 import * as React from 'react';
-import { FlatList, Pressable, ScrollView, StyleSheet, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, View } from 'react-native';
 
 type TDiseaseType = {
   iconName: React.ElementType;
@@ -69,7 +69,6 @@ const extendedDiseases: TDiseaseType[] = [
 ];
 
 export default function ReconsentDiseasePreferencesScreen() {
-  const theme = useTheme();
   const [showExtendedList, setShowExtendedList] = React.useState<boolean>(false);
   const addToPreferences = () => {
     console.log('hi');
@@ -91,40 +90,35 @@ export default function ReconsentDiseasePreferencesScreen() {
   const ShowMore = () => (
     <Pressable onPress={() => setShowExtendedList(true)}>
       <Text rhythm={16} style={styles.showMore} textClass="pMedium">
-        + Show more
+        {i18n.t('reconsent.disease-preferences.show-more')}
       </Text>
     </Pressable>
   );
 
   return (
-    <SafeLayout style={styles.page}>
-      <ScrollView contentContainerStyle={styling.flexGrow} style={{ paddingHorizontal: theme.grid.xxl }}>
-        <ReconsentHeader showBackIcon showDots />
-        <View>
-          <Text rhythm={24} style={styles.center} textClass="h2Light">
-            What matters most to you?
-          </Text>
-          <Text rhythm={16} style={[styles.center, styles.subtitle]} textClass="pLight">
-            Select as many as you like
-          </Text>
-          <FlatList
-            data={showExtendedList ? initialDiseases.concat(extendedDiseases) : initialDiseases}
-            keyExtractor={(disease: TDiseaseType) => disease.name}
-            ListFooterComponent={ShowMore}
-            ListFooterComponentStyle={showExtendedList ? { display: 'none' } : null}
-            renderItem={renderItem}
-            style={{ overflow: 'visible' }}
-          />
-          <View style={styles.infoBox}>
-            <InfoBox text={i18n.t('reconsent.disease-preferences.how-data-used')} />
-          </View>
-        </View>
-      </ScrollView>
-      <ReconsentFooter
-        onPress={() => NavigatorService.navigate('ReconsentDiseaseConfirmation')}
-        title={i18n.t('navigation.next')}
+    <ReconsentScreen
+      buttonOnPress={() => NavigatorService.navigate('ReconsentDiseaseConfirmation')}
+      buttonTitle={i18n.t('navigation.next')}
+    >
+      <ReconsentHeader showBackIcon showDots />
+      <Text rhythm={24} style={styles.center} textClass="h2Light">
+        {i18n.t('reconsent.disease-preferences.title')}
+      </Text>
+      <Text rhythm={16} style={[styles.center, styles.subtitle]} textClass="pLight">
+        {i18n.t('reconsent.disease-preferences.subtitle')}
+      </Text>
+      <FlatList
+        data={showExtendedList ? initialDiseases.concat(extendedDiseases) : initialDiseases}
+        keyExtractor={(disease: TDiseaseType) => disease.name}
+        ListFooterComponent={ShowMore}
+        ListFooterComponentStyle={showExtendedList ? { display: 'none' } : null}
+        renderItem={renderItem}
+        style={{ overflow: 'visible' }}
       />
-    </SafeLayout>
+      <View style={styles.infoBox}>
+        <InfoBox text={i18n.t('reconsent.disease-preferences.how-data-used')} />
+      </View>
+    </ReconsentScreen>
   );
 }
 
